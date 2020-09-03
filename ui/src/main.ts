e03a761457e55e421d6943f05377d0f4eaef2d2f
Martin Maillard <self@martin-maillard.com>
2020-09-03 14:50:06 +0200

    chore: setup buefy/bulma and fontawesome

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,6 +1,21 @@
 import Vue from 'vue'
+import Buefy from 'buefy'
 import App from './App.vue'
 import router from './router'
+
+import { library as iconsLibrary } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import 'buefy/dist/buefy.css'
+
+iconsLibrary.add(fas)
+Vue.component('FontAwesomeIcon', FontAwesomeIcon)
+
+Vue.use(Buefy, {
+  defaultIconPack: 'fas',
+  defaultIconComponent: 'FontAwesomeIcon',
+})
 
 Vue.config.productionTip = false
 
