65297b1107be6d00f66328cb8521d660a3aa01a1
tpluscode <tpluscode@users.noreply.github.com>
2020-09-25 17:13:59 +0200

    refactor(api): authorize explicitly

--- apis/core/lib/auth.ts
+++ apis/core/lib/auth.ts
@@ -10,11 +10,11 @@ import jwksRsa from 'jwks-rsa'
 declare module '@hydrofoil/labyrinth' {
   export interface User {
     sub: string
-    permissions: string[]
+    scope: string[]
   }
 }
 
-const createJwtHandler = (credentialsRequired: boolean, jwksUri: string) => jwt({
+const createJwtHandler = (jwksUri: string) => jwt({
   // Dynamically provide a signing key
   // based on the kid in the header and
   // the signing keys provided by the JWKS endpoint.
@@ -29,7 +29,7 @@ const createJwtHandler = (credentialsRequired: boolean, jwksUri: string) => jwt(
   audience: env.AUTH_AUDIENCE,
   issuer: env.AUTH_ISSUER,
   algorithms: ['RS256'],
-  credentialsRequired,
+  credentialsRequired: false,
 })
 
 function devAuthHandler(req: Request, res: Response, next: NextFunction) {
@@ -40,13 +40,13 @@ function devAuthHandler(req: Request, res: Response, next: NextFunction) {
   }
 
   if (sub) {
-    const permissionHeader = req.headers['x-permission']
-    const permissions = typeof permissionHeader === 'string' ? permissionHeader.split(',').map(s => s.trim()) : permissionHeader || []
+    const permissionHeader = req.headers['x-scope']
+    const scope = typeof permissionHeader === 'string' ? permissionHeader.split(',').map(s => s.trim()) : permissionHeader || []
 
     req.user = {
       id: $rdf.namedNode(sub),
       sub,
-      permissions,
+      scope,
     }
 
     return next()
@@ -63,7 +63,7 @@ export default async () => {
     const response = await fetch(`${env.AUTH_ISSUER}/.well-known/openid-configuration`)
     if (response.ok) {
       const oidcConfig = await response.json()
-      router.use(createJwtHandler(env.production, oidcConfig.jwks_uri))
+      router.use(createJwtHandler(oidcConfig.jwks_uri))
     } else {
       warning('Failed to load OpenID Connect settings from issuer')
     }
