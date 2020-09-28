5043b57101f7971ef41d44b3c2b6a43a236d42a7
Victor Felder <victor@draft.li>
2020-09-28 16:50:10 +0200

    feat(api): CSV file upload

--- apis/core/lib/domain/identifiers.ts
+++ apis/core/lib/domain/identifiers.ts
@@ -1,7 +1,6 @@
 import UrlSlugify from 'url-slugify'
 import $rdf from 'rdf-ext'
 import { NamedNode } from 'rdf-js'
-import { GraphPointer } from 'clownface'
 import env from '../env'
 
 const url = new UrlSlugify()
@@ -10,6 +9,6 @@ export function cubeProject(label: string): NamedNode {
   return $rdf.namedNode(`${env.API_CORE_BASE}cube-project/${url.slugify(label)}`)
 }
 
-export function csvMapping(project: GraphPointer<NamedNode>): NamedNode {
-  return $rdf.namedNode(`${project.value}/csv-mapping`)
+export function csvMapping(filename: string): NamedNode {
+  return $rdf.namedNode(`${env.API_CORE_BASE}csv-mapping/${url.slugify(filename)}`)
 }
