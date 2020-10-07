fc633f919d2365905c7caba3c85e60928ef8f6f8
Victor Felder <victor@draft.li>
2020-10-07 15:54:02 +0200

    feat(csv): read file head

--- apis/core/lib/storage/s3.ts
+++ apis/core/lib/storage/s3.ts
@@ -1,5 +1,4 @@
 import aws from 'aws-sdk'
-import { isReadable } from 'isstream'
 import { Readable } from 'stream'
 import { PromiseResult } from 'aws-sdk/lib/request'
 import env from '../env'
@@ -33,29 +32,99 @@ export async function deleteFile(path: string): Promise<PromiseResult<aws.S3.Del
   }).promise()
 }
 
-export async function loadFile(path: string, stream?: true): Promise<Readable | string | null> {
+export async function loadFileString(path: string): Promise<string | null> {
   const file = await s3.getObject({
     ...defaultS3Options,
     Key: path,
   }).promise()
 
-  if (file.Body && !stream) {
-    return Promise.resolve(file.Body.toString())
+  if (file.Body) {
+    return file.Body.toString()
   }
-
-  if (file.Body instanceof Buffer) {
-    const readable = new Readable()
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    readable._read = () => { }
-    readable.push(file.Body)
-    readable.push(null)
-    return readable
-  }
-
-  if (isReadable(file.Body)) {
-    return file.Body as Readable
-  }
-
-  logError('Could not read file "%s" from S3. It was neither Buffer or Readable', path)
+  logError('Could not read file "%s" from S3.', path)
   return null
+}
+
+export async function loadFileHeadString(path: string, uniqueLines = 20): Promise<string> {
+  let nextByte = 0
+  let bytesToRead = 1024
+  const lines: string[] = []
+  let partialLine = ''
+  let file
+
+  while (true) {
+    if (file?.ContentLength) {
+      // range shouldn't read beyond the end of file
+      if (file.ContentLength > (nextByte + bytesToRead)) {
+        bytesToRead = file.ContentLength - nextByte
+      }
+    }
+
+    try {
+      file = await s3.getObject({
+        ...defaultS3Options,
+        Key: path,
+        Range: `bytes=${nextByte}-${nextByte + bytesToRead}`,
+      }).promise()
+    } catch (err) {
+      if (err.code === 'InvalidRange') {
+        // range is bigger than the whole file, so reduce the range
+        if (nextByte === 0) {
+          bytesToRead /= 2
+          continue
+        }
+        if (file?.ContentLength && nextByte > file.ContentLength) {
+          // we read the whole file
+          break
+        }
+      } else {
+        throw err
+      }
+    }
+    if (!file || !file.Body) {
+      // end of file
+      break
+    }
+
+    let string = file.Body?.toString() || ''
+    if (!string.includes('\n')) {
+      // continue reading until we reach the first \n
+      partialLine += string
+      nextByte += bytesToRead
+      continue
+    }
+    if (partialLine) {
+      string = partialLine + string
+      partialLine = ''
+    }
+
+    // carry the beginning of the last line over to next iteration
+    if (!string.endsWith('\n')) {
+      partialLine = string.substring(string.lastIndexOf('\n') + '\n'.length)
+      string = string.substring(0, string.lastIndexOf('\n'))
+    }
+
+    const newLines = string.split('\n')
+    // always keep the 1st line
+    if (lines.length === 0) {
+      lines.push(newLines.shift() as string)
+    }
+
+    // only keep unique lines otherwise
+    newLines.forEach((line) => {
+      if (!lines.includes(line)) {
+        lines.push(line)
+      }
+    })
+
+    // got all we need
+    if (lines.length >= uniqueLines) {
+      lines.splice(uniqueLines)
+      break
+    }
+
+    nextByte += bytesToRead
+  }
+
+  return lines.join('\n')
 }
--- apis/core/test/domain/csv/parse.test.ts
+++ apis/core/test/domain/csv/parse.test.ts
@@ -3,7 +3,7 @@ import { expect } from 'chai'
 import { promises as fs } from 'fs'
 import { resolve } from 'path'
 import { sniffParse } from '../../../lib/domain/csv'
-import { saveFile, loadFile } from '../../../lib/storage/s3'
+import { saveFile, loadFileString, loadFileHeadString } from '../../../lib/storage/s3'
 
 describe('domain/csv/parse', () => {
   it('sniffs and parses', async () => {
@@ -21,12 +21,27 @@ describe('domain/csv/parse', () => {
     const path = resolve(__dirname, '../../fixtures/CH_yearly_air_immission_aggregation_id.csv')
     const fileContent = await fs.readFile(path)
     await saveFile('test/CH_yearly_air_immission_aggregation_id.csv', fileContent.toString())
-    const input = await loadFile('test/CH_yearly_air_immission_aggregation_id.csv')
+    const input = await loadFileString('test/CH_yearly_air_immission_aggregation_id.csv')
 
-    const { header, rows } = await sniffParse(input)
+    const { header, rows } = await sniffParse(String(input))
     const [lastRow] = rows.slice(-1)
 
     expect(header).to.deep.eq(['aggregation_id', 'aggregation_name_de', 'aggregation_name_fr', 'aggregation_name_it', 'aggregation_name_en'])
     expect(lastRow).to.deep.eq(['dosisaot40f', 'Dosis AOT40f', 'Dose AOT40f', 'Dose AOT40f', 'Dosis AOT40f'])
+  })
+
+  it('reads parts of a file from s3', async () => {
+    const path = resolve(__dirname, '../../fixtures/CH_yearly_air_immission_basetable.csv')
+    const fileContent = await fs.readFile(path)
+    await saveFile('test/CH_yearly_air_immission_basetable.csv', fileContent.toString())
+
+    const input1 = await loadFileString('test/CH_yearly_air_immission_basetable.csv')
+    const input2 = await loadFileHeadString('test/CH_yearly_air_immission_basetable.csv')
+
+    expect(input1).not.to.eq(input2)
+
+    const lines1 = input1?.split('\n') || []
+    const [firstLine2] = input2?.split('\n')
+    expect(lines1[0]).to.eq(firstLine2)
   })
 })
