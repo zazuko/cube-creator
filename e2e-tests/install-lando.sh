835c0d8bdb331c9146ec14716390ffcd6626da2a
Luca Furrer <luca.s.furrer@gmail.com>
2020-10-14 17:40:42 +0200

    fix: use latest version of lando

--- e2e-tests/install-lando.sh
+++ e2e-tests/install-lando.sh
@@ -4,6 +4,6 @@ sudo apt-get -y update || true
 sudo apt-get -y install cgroup-bin curl
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
-curl -fsSL -o /tmp/lando-latest.deb https://github.com/lando/lando/releases/download/v3.0.11/lando-v3.0.11.deb
+curl -fsSL -o /tmp/lando-latest.deb https://github.com/lando/lando/releases/download/v3.0.14/lando-v3.0.14.deb
 sudo dpkg -i /tmp/lando-latest.deb
 lando version
