ef0c81ce1d2bc966b84f2c34693a50977e28786d
Luca Furrer <luca.s.furrer@gmail.com>
2020-10-15 13:12:41 +0200

    feat(csv): error if file added twice to same project

---
+++ apis/core/lib/domain/queries/csv-source.ts
@@ -0,0 +1,21 @@
+import { NamedNode } from 'rdf-js'
+import { ASK } from '@tpluscode/sparql-builder'
+import { cc } from '@cube-creator/core/namespace'
+import { schema } from '@tpluscode/rdf-ns-builders'
+import { client } from './query-client'
+
+export async function sourceWithFilenameExists(csvMapping: NamedNode, fileName: string): Promise<boolean> {
+  const result = await ASK`
+      GRAPH ${csvMapping} 
+      {
+        ${csvMapping} ${cc.csvSource} ?source
+      }
+      GRAPH ?source
+      {
+          ?source ${schema.name} "${fileName}"
+      }
+      `
+    .execute(client.query)
+
+  return result
+}
