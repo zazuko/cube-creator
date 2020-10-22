03a5a7d73a6b2e151102184c1cb34be6fce96cfa
Martin Maillard <self@martin-maillard.com>
2020-10-22 15:26:01 +0200

    feat: use vue components are shaperone property/object renderers
    This will give us more control over the styling of our forms.
    
    One downside I still couldn't figure out is that there are a lot of
    superfluous HTML tags in the DOM, which makes styling way harder than it
    should be. But at least we can have a consistent look and change the
    rendering as needed.

---
+++ ui/src/forms/FormProperty.vue
@@ -0,0 +1,34 @@
+<template>
+  <b-field v-if="property" :label="property.name">
+    <b-field v-if="property.selectedEditor">
+      <render-wc-func :f="renderMultiEditor" :args="[null]" />
+    </b-field>
+    <div v-else v-for="object in property.objects" :key="object.id">
+      <render-wc-func :f="renderObject" :args="[object]" />
+    </div>
+    <div v-if="!property.selectedEditor && property.canAdd">
+      <b-tooltip label="Add value">
+        <b-button icon-left="plus" @click="actions.addObject" type="is-white" />
+      </b-tooltip>
+    </div>
+  </b-field>
+</template>
+
+<script lang="ts">
+import { Prop, Component, Vue } from 'vue-property-decorator'
+import { PropertyObjectState, PropertyState } from '@hydrofoil/shaperone-core/models/forms'
+import { TemplateResult } from 'lit-element'
+import RenderWcFunc from './RenderWcFunc.vue'
+
+@Component({
+  components: {
+    RenderWcFunc,
+  }
+})
+export default class extends Vue {
+  @Prop() property!: PropertyState;
+  @Prop() actions!: any;
+  @Prop() renderObject!: (object: PropertyObjectState) => TemplateResult;
+  @Prop() renderMultiEditor!: () => TemplateResult;
+}
+</script>
