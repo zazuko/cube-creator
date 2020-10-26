8ad804a3390c7c31acb13b2252fedd0fc51322dc
Luca Furrer <luca.s.furrer@gmail.com>
2020-10-26 16:11:07 +0100

    chore: clean up imports

--- apis/core/lib/domain/csv-source/delete.ts
+++ apis/core/lib/domain/csv-source/delete.ts
@@ -1,10 +1,10 @@
-import { NamedNode, Quad } from 'rdf-js'
+import { NamedNode } from 'rdf-js'
 import { ResourceStore } from '../../ResourceStore'
 import { resourceStore } from '../resources'
 import { deleteFile } from '../../storage/s3'
 import { schema } from '@tpluscode/rdf-ns-builders'
 import { cc } from '@cube-creator/core/namespace'
-import { quad } from 'rdf-ext'
+import $rdf from 'rdf-ext'
 
 interface DeleteSourceCommand {
   resource: NamedNode
@@ -32,7 +32,7 @@ export async function deleteSource({
   const csvMapping = csvSource.out(cc.csvMapping).term
   if (csvMapping) {
     const csvMappingGraph = await store.get(csvMapping.value)
-    csvMappingGraph.dataset.delete(quad(csvMappingGraph.term, cc.csvSource, csvSource.term))
+    csvMappingGraph.dataset.delete($rdf.quad(csvMappingGraph.term, cc.csvSource, csvSource.term))
   }
 
   // Delete Graph
