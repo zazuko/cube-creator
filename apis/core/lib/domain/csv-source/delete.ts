db5cb3a0b4f4a8041bf75563d40a7bd029849055
Luca Furrer <luca.s.furrer@gmail.com>
2020-10-26 15:48:50 +0100

    fix: delete link from csvMapping to deleted csvSource

--- apis/core/lib/domain/csv-source/delete.ts
+++ apis/core/lib/domain/csv-source/delete.ts
@@ -1,8 +1,10 @@
-import { NamedNode } from 'rdf-js'
+import { NamedNode, Quad } from 'rdf-js'
 import { ResourceStore } from '../../ResourceStore'
 import { resourceStore } from '../resources'
 import { deleteFile } from '../../storage/s3'
 import { schema } from '@tpluscode/rdf-ns-builders'
+import { cc } from '@cube-creator/core/namespace'
+import { quad } from 'rdf-ext'
 
 interface DeleteSourceCommand {
   resource: NamedNode
@@ -24,6 +26,13 @@ export async function deleteSource({
     ?.value
   if (path) {
     await deleteFile(path)
+  }
+
+  // Delete links from in csv-mapping
+  const csvMapping = csvSource.out(cc.csvMapping).term
+  if (csvMapping) {
+    const csvMappingGraph = await store.get(csvMapping.value)
+    csvMappingGraph.dataset.delete(quad(csvMappingGraph.term, cc.csvSource, csvSource.term))
   }
 
   // Delete Graph
