b61429a6af87473207c2ad8b6049bd20baf7f15b
Martin Maillard <self@martin-maillard.com>
2020-10-27 13:59:26 +0100

    refactor: return validation response directly from middleware

--- apis/core/lib/errors/index.ts
+++ apis/core/lib/errors/index.ts
@@ -1,8 +1,6 @@
 import { ForbiddenMapper, UnauthorizedMapper } from './auth'
-import { ValidationErrorMapper } from './validation'
 
 export const errorMappers = [
   new ForbiddenMapper(),
   new UnauthorizedMapper(),
-  new ValidationErrorMapper(),
 ]
