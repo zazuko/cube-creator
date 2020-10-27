8e7bc474ba4b4c9ef1e46427b11bd0787c6cb4bc
Martin Maillard <self@martin-maillard.com>
2020-10-27 16:23:28 +0100

    refactor: use ParsingClient instead of StreamClient

--- apis/core/lib/domain/queries/resources-types.ts
+++ apis/core/lib/domain/queries/resources-types.ts
@@ -1,10 +1,10 @@
 import { Term, Quad } from 'rdf-js'
 import { CONSTRUCT } from '@tpluscode/sparql-builder'
 import { rdf } from '@tpluscode/rdf-ns-builders'
-import { streamClient } from '../../query-client'
+import { parsingClient } from '../../query-client'
 
-export async function loadResourcesTypes(ids: Term[], client = streamClient): Promise<Quad[]> {
-  const stream = await CONSTRUCT`?resource ${rdf.type} ?type`
+export async function loadResourcesTypes(ids: Term[], client = parsingClient): Promise<Quad[]> {
+  return CONSTRUCT`?resource ${rdf.type} ?type`
     .WHERE`
       values ?resource {
         ${ids}
@@ -13,20 +13,4 @@ export async function loadResourcesTypes(ids: Term[], client = streamClient): Pr
     .WHERE`GRAPH ?g {
         ?resource a ?type .
     }`.execute(client.query)
-
-  return new Promise((resolve, reject) => {
-    const quads: Quad[] = []
-
-    stream.on('data', (quad) => {
-      quads.push(quad)
-    })
-
-    stream.on('end', () => {
-      resolve(quads)
-    })
-
-    stream.on('error', err => {
-      reject(err)
-    })
-  })
 }
