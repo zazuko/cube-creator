fff861199c06c7cf1039159693ab7328e5f7476a
tpluscode <tpluscode@users.noreply.github.com>
2020-11-02 09:10:49 +0100

    typescript improvements

--- typings/rdf-validate-shacl/index.d.ts
+++ typings/rdf-validate-shacl/index.d.ts
@@ -2,26 +2,28 @@ declare module 'rdf-validate-shacl' {
   import type { DatasetCore, Term } from 'rdf-js'
   import type { GraphPointer } from 'clownface'
 
-  export interface ValidationResult {
-    message: Term[]
-    path: Term | null
-    focusNode: Term | null
-    severity: Term | null
-    sourceConstraintComponent: Term | null
-    sourceShape: Term | null
-    cf: GraphPointer
-  }
+  namespace SHACLValidator {
+    interface ValidationResult {
+      message: Term[]
+      path: Term | null
+      focusNode: Term | null
+      severity: Term | null
+      sourceConstraintComponent: Term | null
+      sourceShape: Term | null
+      cf: GraphPointer
+    }
 
-  export interface ValidationReport {
-    conforms: boolean
-    results: ValidationResult[]
-    dataset: DatasetCore
+    interface ValidationReport {
+      conforms: boolean
+      results: ValidationResult[]
+      dataset: DatasetCore
+    }
   }
 
   class SHACLValidator {
     constructor(shapes: DatasetCore)
 
-    validate(dataset: DatasetCore): ValidationReport
+    validate(dataset: DatasetCore): SHACLValidator.ValidationReport
   }
 
   export = SHACLValidator
