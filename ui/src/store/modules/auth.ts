2177921fe0c47d1a146ea62513300153c6d725ef
Quentin Gliech <quentingliech@gmail.com>
2020-11-03 21:01:25 +0100

    fix: use cookie to store OIDC state
    this allows the app to work in private browsing sessions in Firefox

--- ui/src/store/modules/auth.ts
+++ ui/src/store/modules/auth.ts
@@ -1,14 +1,24 @@
+import { CookieStorage } from 'cookie-storage'
 import { Module } from 'vuex'
 import { VuexOidcClientSettings, VuexOidcState, vuexOidcCreateStoreModule } from 'vuex-oidc'
+import { WebStorageStateStore } from 'oidc-client'
 import { RootState } from '../types'
 
 const oidc = window.APP_CONFIG.oidc
 const base = new URL(process.env.BASE_URL || '/', window.location.toString())
 
+const cookieStorage = new CookieStorage({
+  path: base.pathname,
+  domain: base.hostname,
+  secure: base.protocol === 'https:',
+  sameSite: 'Strict',
+})
+
 const oidcSettings: VuexOidcClientSettings = {
   redirectUri: new URL('oidc-callback', base).toString(),
   postLogoutRedirectUri: new URL('logout', base).toString(),
   responseType: 'code',
+  stateStore: new WebStorageStateStore({ store: cookieStorage }),
   ...oidc,
 }
 
