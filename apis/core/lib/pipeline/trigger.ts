7015bec63c1c3bff01662e561019e6933b71b909
tpluscode <tpluscode@users.noreply.github.com>
2020-11-06 12:53:25 +0100

    feat: added GitHub trigger request

--- apis/core/lib/pipeline/trigger.ts
+++ apis/core/lib/pipeline/trigger.ts
@@ -1,33 +1,53 @@
 import { URLSearchParams } from 'url'
 import { NamedNode } from 'rdf-js'
-import fetch from 'node-fetch'
+import fetch, { RequestInit } from 'node-fetch'
 import env from '@cube-creator/core/env'
 
 const pipelineURI = env.PIPELINE_URI
 
-export async function local(job: NamedNode): Promise<void> {
-  if (!job) {
-    throw new Error('Job URI missing')
+function trigger(triggerRequestInit: (job: NamedNode) => RequestInit) {
+  return async (job: NamedNode) => {
+    if (!job) {
+      throw new Error('Job URI missing')
+    }
+
+    const res = await fetch(pipelineURI, triggerRequestInit(job))
+
+    if (res.status !== 201) {
+      const message = await res.text()
+      throw new Error(`Pipeline failed: ${message}`)
+    }
   }
+}
+
+export const local = trigger(job => {
   const form = new URLSearchParams({
     JOB_URI: job.value,
   })
 
-  const res = await fetch(pipelineURI, {
+  return {
     method: 'POST',
     body: form,
-  })
-
-  if (res.status !== 201) {
-    const message = await res.text()
-    throw new Error(`Pipeline failed: ${message}`)
   }
-}
+})
 
 export async function gitlab(job: NamedNode): Promise<void> {
   throw new Error('Not implemented')
 }
 
-export async function github(job: NamedNode): Promise<void> {
-  throw new Error('Not implemented')
-}
+export const github = trigger(job => {
+  const body = {
+    ref: 'master',
+    inputs: {
+      job: job.value,
+    },
+  }
+
+  return {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      accept: 'application/vnd.github.v3+json',
+    },
+  }
+})
