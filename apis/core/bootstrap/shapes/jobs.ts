a1de43e1a387a9b0e9258bb23330bfb16768ff19
tpluscode <tpluscode@users.noreply.github.com>
2020-11-09 09:52:33 +0100

    fix: added a job trigger shape

--- apis/core/bootstrap/shapes/jobs.ts
+++ apis/core/bootstrap/shapes/jobs.ts
@@ -1,6 +1,25 @@
 import { turtle } from '@tpluscode/rdf-string'
 import { shape } from '@cube-creator/core/namespaces/shapes'
-import { hydra, rdfs, schema, sh } from '@tpluscode/rdf-ns-builders'
+import { dcterms, hydra, rdfs, schema, sh } from '@tpluscode/rdf-ns-builders'
+import env from '@cube-creator/core/env'
+
+let jobTriggerProperties = turtle``
+if (env.PIPELINE_TYPE === 'github') {
+  jobTriggerProperties = turtle`${sh.property} [
+    ${sh.path} ${dcterms.identifier} ;
+    ${sh.name} "GitHub Personal Access Token" ;
+    ${sh.minLength} 1 ;
+    ${sh.minCount} 1 ;
+    ${sh.manCount} 1 ;
+  ] ;`
+}
+
+export const JobTriggerShape = turtle`
+${shape('job/trigger')} {
+  ${shape('job/trigger')} a ${sh.NodeShape} , ${hydra.Resource} ;
+    ${jobTriggerProperties}
+  .
+}`
 
 export const JobUpdateShape = turtle`
 ${shape('job/update')} {
