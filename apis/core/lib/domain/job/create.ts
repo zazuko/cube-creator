31c747172d8366b1b93a520e614f105a880de612
tpluscode <tpluscode@users.noreply.github.com>
2020-11-09 12:33:15 +0100

    refactor: creating job

--- apis/core/lib/domain/job/create.ts
+++ apis/core/lib/domain/job/create.ts
@@ -1,10 +1,11 @@
 import { cc } from '@cube-creator/core/namespace'
 import { GraphPointer } from 'clownface'
 import { NamedNode } from 'rdf-js'
+import * as Job from '@cube-creator/model/Job'
+import { CsvMapping, Project } from '@cube-creator/model'
 import { ResourceStore } from '../../ResourceStore'
 import { resourceStore } from '../resources'
 import * as id from '../identifiers'
-import { rdfs } from '@tpluscode/rdf-ns-builders'
 
 interface StartTransformationCommand {
   resource: NamedNode
@@ -16,28 +17,25 @@ export async function createJob({
   store = resourceStore(),
 }: StartTransformationCommand): Promise<GraphPointer<NamedNode>> {
   const jobCollection = (await store.get(resource))!
-  const csvMapping = jobCollection.out(cc.csvMapping).term
+  const project = await store.getResource<Project>(jobCollection.out(cc.project).term)
+  const csvMapping = await store?.getResource<CsvMapping>(project?.csvMapping?.id)
 
-  if (!csvMapping || csvMapping.termType !== 'NamedNode') {
-    throw new Error('CSV-Mapping is missing')
+  if (!project) {
+    throw new Error('Project not found from jobs collection')
   }
 
-  const csvMappingResource = (await store.get(csvMapping))!
-  const project = csvMappingResource.out(cc.project).term
-  if (project?.termType !== 'NamedNode') {
-    throw new Error('Project is missing')
+  if (!csvMapping) {
+    throw new Error('CSV Mapping not found from project')
   }
 
-  const projectResource = (await store.get(project))!
-  const cubeGraph = projectResource.out(cc.cubeGraph)
-
-  const job = await store.createMember(jobCollection.term, id.job(jobCollection))
-
-  job.addOut(cc.cubeGraph, cubeGraph)
-    .addOut(cc.tables, csvMappingResource.out(cc.tables))
-    .addOut(rdfs.label, projectResource.out(rdfs.label))
+  const jobPointer = await store.createMember(jobCollection.term, id.job(jobCollection))
+  Job.create(jobPointer, {
+    cubeGraph: project.cubeGraph,
+    label: 'Transformation job',
+    tableCollection: csvMapping.tableCollection,
+  })
 
   await store.save()
 
-  return job
+  return jobPointer
 }
