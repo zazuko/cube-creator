8dab118d74a25f3c31e9c187986cf1d4b8190b18
tpluscode <tpluscode@users.noreply.github.com>
2020-11-10 09:45:59 +0100

    fix: exclude cc:cube from cube shape

--- cli/test/lib/commands/transform.test.ts
+++ cli/test/lib/commands/transform.test.ts
@@ -2,13 +2,14 @@ import env from '@cube-creator/core/env'
 import { before, describe, it } from 'mocha'
 import { expect } from 'chai'
 import $rdf from 'rdf-ext'
-import { CONSTRUCT } from '@tpluscode/sparql-builder'
+import { ASK, CONSTRUCT } from '@tpluscode/sparql-builder'
 import debug from 'debug'
 import { Hydra } from 'alcaeus/node'
 import { rdfs, schema, sh, xsd } from '@tpluscode/rdf-ns-builders'
 import transform from '../../../lib/commands/transform'
 import { setupEnv } from '../../support/env'
 import { client, insertTestData } from '@cube-creator/testing/lib'
+import { cc } from '@cube-creator/core/namespace'
 
 describe('lib/commands/transform', function () {
   this.timeout(200000)
@@ -43,6 +44,25 @@ describe('lib/commands/transform', function () {
       .execute(client.query))
 
     expect(dataset.toCanonical()).to.matchSnapshot(this)
+  })
+
+  it('does not output cc:cube as dimension property', async () => {
+    // given
+    const runner = transform($rdf.namedNode('urn:pipeline:cube-creator#Main'), debug('test'))
+
+    // when
+    await runner({
+      to: 'graph-store',
+      debug: true,
+      job: `${env.API_CORE_BASE}project/ubd/csv-mapping/jobs/test-job`,
+      executionUrl,
+    })
+
+    // then
+    const hasCubeProperty = await ASK`<https://environment.ld.admin.ch/foen/ubd/28/shape/> ${sh.property}/${sh.path} ${cc.cube} .`
+      .FROM($rdf.namedNode(`${env.API_CORE_BASE}cube-project/ubd/cube-data`))
+      .execute(client.query)
+    expect(hasCubeProperty).to.eq(false)
   })
 
   it('updates job when pipeline ends successfully', async function () {
