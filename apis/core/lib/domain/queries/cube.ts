8abd234caec5de68ab19bfd1cc4dc915c6443266
tpluscode <tpluscode@users.noreply.github.com>
2020-11-17 15:01:39 +0100

    refactor: iterate the stream instead of createing dataset twice

--- apis/core/lib/domain/queries/cube.ts
+++ apis/core/lib/domain/queries/cube.ts
@@ -1,11 +1,12 @@
 import { GraphPointer } from 'clownface'
 import { CONSTRUCT } from '@tpluscode/sparql-builder'
-import ParsingClient from 'sparql-http-client/ParsingClient'
+import StreamClient from 'sparql-http-client/StreamClient'
 import { cc, cube } from '@cube-creator/core/namespace'
-import { Quad } from 'rdf-js'
+import { Stream } from 'rdf-js'
 import { schema } from '@tpluscode/rdf-ns-builders'
+import { Readable } from 'stream'
 
-export function loadCubeShapes(dataset: GraphPointer, client: ParsingClient): Promise<Quad[]> {
+export function loadCubeShapes(dataset: GraphPointer, client: StreamClient): Promise<Stream & Readable> {
   return CONSTRUCT`
     ?s ?p ?o .
     ?cube ${cube.observationConstraint} ?shape .
