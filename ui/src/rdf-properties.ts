6e014ab71ec1d0813b5b6e7b6d1df5f852ad680d
Martin Maillard <self@martin-maillard.com>
2020-11-17 10:35:10 +0100

    chore: fix all vocab factories loaded in vendor bundle

--- ui/src/rdf-properties.ts
+++ ui/src/rdf-properties.ts
@@ -1,22 +1,23 @@
 import $rdf from '@rdf-esm/dataset'
-import { DataFactory, Quad } from 'rdf-js'
+import { Quad } from 'rdf-js'
 import { expand as _expand } from '@zazuko/rdf-vocabularies/expand'
 import { shrink as _shrink } from '@zazuko/rdf-vocabularies/shrink'
-import * as _vocabFactories from '@zazuko/rdf-vocabularies/datasets'
+import { vocabularies } from '@zazuko/rdf-vocabularies/vocabularies'
+import prefixes from '@zazuko/rdf-vocabularies/prefixes'
 import { rdf } from '@tpluscode/rdf-ns-builders'
 
 const relevantPrefixes = ['rdfs', 'schema', 'qb', 'sdmx', 'dcterms', 'dc11', 'skos', 'skosxl', 'xkos', 'xsd', 'wgs']
-const vocabFactories = _vocabFactories as Record<string, (factory: DataFactory) => Quad[]>
 
 export async function loadCommonProperties (): Promise<string[]> {
-  const quads = Object.keys(vocabFactories)
-    .filter((prefix) => relevantPrefixes.includes(prefix))
-    .flatMap((prefix: string) => vocabFactories[prefix]($rdf))
+  const vocabs = await vocabularies({ only: relevantPrefixes })
 
-  const dataset = $rdf.dataset(quads)
+  return Object.entries(vocabs).flatMap(([prefix, dataset]) => {
+    const baseIRI = prefixes[prefix]
+    const graph = $rdf.namedNode(baseIRI)
+    const properties = dataset.match(null, rdf.type, rdf.property, graph)
 
-  return [...dataset.match(null, rdf.type, rdf.Property)]
-    .map((property: Quad) => shrink(property.subject.value))
+    return properties.toArray().map((property: Quad) => shrink(property.subject.value))
+  })
 }
 
 export function expand (uri: string): string {
