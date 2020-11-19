416130007894c1a1d5a96c30062fa4bda39cbd0e
Martin Maillard <self@martin-maillard.com>
2020-11-19 16:54:07 +0100

    chore: fix typing issues

--- packages/model/DimensionMetadata.ts
+++ packages/model/DimensionMetadata.ts
@@ -1,12 +1,12 @@
+import * as Rdfs from '@rdfine/rdfs'
 import { Constructor, namespace, property, RdfResource } from '@tpluscode/rdfine'
 import { Mixin } from '@tpluscode/rdfine/lib/ResourceFactory'
-import { RdfResourceCore } from '@tpluscode/rdfine/RdfResource'
 import { Literal, NamedNode } from 'rdf-js'
 import { schema } from '@tpluscode/rdf-ns-builders'
 import { cc } from '@cube-creator/core/namespace'
 import { initializer } from './lib/initializer'
 
-export interface DimensionMetadata extends RdfResourceCore {
+export interface DimensionMetadata extends RdfResource {
   about?: NamedNode
   name: Literal[]
 }
@@ -15,9 +15,9 @@ export interface DimensionMetadataCollection extends RdfResource {
   hasPart: Array<DimensionMetadata>
 }
 
-function DimensionMetadataMixin<Base extends Constructor>(Resource: Base) {
+function DimensionMetadataMixin<Base extends Constructor>(base: Base): Mixin {
   @namespace(schema)
-  class Impl extends Resource implements DimensionMetadata {
+  class Impl extends Rdfs.ResourceMixin(base) implements Partial<DimensionMetadata> {
     @property()
     about!: NamedNode
 
