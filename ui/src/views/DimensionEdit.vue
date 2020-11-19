0de0fbed97816c1664f7b7981df175739995be6e
Martin Maillard <self@martin-maillard.com>
2020-11-19 16:04:28 +0100

    feat(ui): add route to edit dimension metadata

---
+++ ui/src/views/DimensionEdit.vue
@@ -0,0 +1,98 @@
+<template>
+  <side-pane :is-open="true" :title="title" @close="onCancel">
+    <b-field label="Dimension property">
+      <property-display v-if="dimension.about" :term="dimension.about" />
+    </b-field>
+    <hydra-operation-form
+      v-if="operation"
+      :operation="operation"
+      :resource="resource"
+      :shape="shape"
+      :error="error"
+      :is-submitting="isSubmitting"
+      @submit="onSubmit"
+      @cancel="onCancel"
+    />
+  </side-pane>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { RuntimeOperation } from 'alcaeus'
+import { Shape } from '@rdfine/shacl'
+import { GraphPointer } from 'clownface'
+import { namespace } from 'vuex-class'
+import { Dataset, DimensionMetadata } from '@cube-creator/model'
+import SidePane from '@/components/SidePane.vue'
+import HydraOperationForm from '@/components/HydraOperationForm.vue'
+import PropertyDisplay from '@/components/PropertyDisplay.vue'
+import { api } from '@/api'
+import { APIErrorValidation, ErrorDetails } from '@/api/errors'
+
+const projectNS = namespace('project')
+
+@Component({
+  components: { SidePane, HydraOperationForm, PropertyDisplay },
+})
+export default class extends Vue {
+  @projectNS.Getter('findDimension') findDimension!: (id: string) => DimensionMetadata
+
+  resource: GraphPointer | null = null;
+  shape: Shape | null = null;
+  error: ErrorDetails | null = null;
+  isSubmitting = false
+
+  async mounted (): Promise<void> {
+    if (this.operation) {
+      this.shape = await api.fetchOperationShape(this.operation)
+    }
+
+    this.resource = this.dimension.pointer
+  }
+
+  get dimension (): DimensionMetadata {
+    return this.findDimension(this.$route.params.dimensionId)
+  }
+
+  get operation (): RuntimeOperation | null {
+    return this.dimension.actions.edit
+  }
+
+  get title (): string {
+    return this.operation?.title ?? 'Error: Missing operation'
+  }
+
+  async onSubmit (): Promise<void> {
+    this.error = null
+    this.isSubmitting = true
+
+    try {
+      await this.$store.dispatch('api/invokeSaveOperation', {
+        operation: this.operation,
+        resource: this.resource,
+      })
+
+      this.$store.dispatch('project/fetchDimensionMetadataCollection')
+
+      this.$store.dispatch('app/showMessage', {
+        message: 'Dimension metadata was saved',
+        type: 'is-success',
+      })
+
+      this.$router.push({ name: 'CubeDesigner' })
+    } catch (e) {
+      this.error = e.details ?? { detail: e.toString() }
+
+      if (!(e instanceof APIErrorValidation)) {
+        console.error(e)
+      }
+    } finally {
+      this.isSubmitting = false
+    }
+  }
+
+  onCancel (): void {
+    this.$router.push({ name: 'CubeDesigner' })
+  }
+}
+</script>
