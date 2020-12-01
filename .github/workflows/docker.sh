d588bb1ccbc72823420e85bb696f56f30929cc0d
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2020-12-01 13:22:59 +0100

    fix: typo

--- .github/workflows/docker.sh
+++ .github/workflows/docker.sh
@@ -5,7 +5,7 @@ if [[ $GITHUB_REF == refs/tags/* ]]; then
   VERSION=${GITHUB_REF#refs/tags/}
 elif [[ $GITHUB_REF == refs/heads/* ]]; then
   VERSION=$(echo ${GITHUB_REF#refs/heads/} | sed -r 's#/+#-#g')
-  if [ "$DEFUALT_BRANCH" = "$VERSION" ]; then
+  if [ "$DEFAULT_BRANCH" = "$VERSION" ]; then
     VERSION=edge
   fi
 elif [[ $GITHUB_REF == refs/pull/* ]]; then
