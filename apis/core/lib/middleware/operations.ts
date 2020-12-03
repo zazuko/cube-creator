efcb006d8b02240af4e476067edf2e256479b41d
tpluscode <tpluscode@users.noreply.github.com>
2020-12-03 11:45:37 +0100

    chore: add logs of filtered operations

--- apis/core/lib/middleware/operations.ts
+++ apis/core/lib/middleware/operations.ts
@@ -1,8 +1,16 @@
 import { RequestHandler } from 'express'
-import clownface from 'clownface'
+import clownface, { GraphPointer } from 'clownface'
 import asyncMiddleware from 'middleware-async'
 import { hydra, rdf } from '@tpluscode/rdf-ns-builders'
 import TermSet from '@rdfjs/term-set'
+import { log } from '../log'
+
+function logRemovedOperation(operation: GraphPointer) {
+  const method = operation.out(hydra.method).value
+  const types = operation.out(rdf.type).values
+  const clas = operation.in(hydra.supportedOperation).values
+  log('Removing %s operation %o supported by %o', method, types, clas)
+}
 
 export const expectsDisambiguate: RequestHandler = asyncMiddleware(async (req, res, next) => {
   if (req.hydra.operations.length < 2) {
@@ -18,7 +26,11 @@ export const expectsDisambiguate: RequestHandler = asyncMiddleware(async (req, r
       .has(rdf.type, hydra.Class)
       .terms
 
-    return new Set(expectedClasses.filter(type => !resourceTypes.has(type))).size === 0
+    const shouldRemove = new Set(expectedClasses.filter(type => !resourceTypes.has(type))).size !== 0
+    if (shouldRemove && log.enabled) {
+      logRemovedOperation(operation)
+    }
+    return !shouldRemove
   })
   next()
 })
@@ -34,7 +46,13 @@ export const preferHydraCollection: RequestHandler = (req, res, next) => {
     .filter(({ operation }) => new TermSet(operation.in(hydra.supportedOperation).terms).has(hydra.Resource))
 
   if (collectionOperations) {
-    req.hydra.operations = req.hydra.operations.filter(value => !resourceOperations.includes(value))
+    req.hydra.operations = req.hydra.operations.filter(value => {
+      const shouldRemove = resourceOperations.includes(value)
+      if (shouldRemove && log.enabled) {
+        logRemovedOperation(value.operation)
+      }
+      return !shouldRemove
+    })
   }
 
   next()
