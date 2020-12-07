2f79ad962e150c4a033052ff15b97902d19a0c2d
Quentin Gliech <quentingliech@gmail.com>
2020-12-07 19:12:50 +0100

    feat: setup Sentry

--- nginx/template-config.sh
+++ nginx/template-config.sh
@@ -8,5 +8,7 @@ TEMPLATE="$WEB_ROOT/config.js.template"
 : "$AUTH_ISSUER"
 : "$AUTH_CLIENT_ID"
 : "$API_CORE_BASE"
+: "$SENTRY_DSN"
+: "$SENTRY_ENVIRONMENT"
 
 envsubst < $TEMPLATE > $OUTPUT
--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import * as Sentry from '@sentry/vue'
+import { Integrations } from '@sentry/tracing'
 import Buefy from 'buefy'
 import App from './App.vue'
 import router from './router'
@@ -23,6 +25,25 @@ Vue.use(Buefy, {
   defaultTooltipType: 'is-light',
   defaultTooltipDelay: 200,
 })
+
+if (window.APP_CONFIG.sentry) {
+  Sentry.init({
+    Vue: Vue,
+    dsn: window.APP_CONFIG.sentry.dsn,
+    environment: window.APP_CONFIG.sentry.environment,
+    release: process.env.VUE_APP_SENTRY_RELEASE,
+    attachProps: true,
+    tracesSampleRate: 1,
+    integrations: [
+      new Integrations.BrowserTracing({
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router)
+      })
+    ],
+    tracingOptions: {
+      trackComponents: true
+    }
+  })
+}
 
 Vue.config.productionTip = false
 
