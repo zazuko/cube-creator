216ea24ac4864d7aae74fb883b79691a5ca7aefe
Luca Furrer <luca.s.furrer@gmail.com>
2020-12-07 13:55:07 +0100

    test: split up expect statements

--- cli/test/lib/commands/publish.test.ts
+++ cli/test/lib/commands/publish.test.ts
@@ -9,7 +9,7 @@ import { dcat, dcterms, rdf, schema, sh, vcard, xsd } from '@tpluscode/rdf-ns-bu
 import { setupEnv } from '../../support/env'
 import { client, insertTestData } from '@cube-creator/testing/lib'
 import { cube, scale } from '@cube-creator/core/namespace'
-import clownface from 'clownface'
+import clownface, { AnyPointer } from 'clownface'
 import publish from '../../../lib/commands/publish'
 import namespace from '@rdfjs/namespace'
 
@@ -27,124 +27,138 @@ describe('lib/commands/publish', function () {
     await insertTestData('fuseki/sample-ubd.trig')
   })
 
-  it('produces triples', async function () {
+  describe('produces triples', async function () {
+    let cubePointer: AnyPointer
+    before(async () => {
     // given
-    const runner = publish($rdf.namedNode('urn:pipeline:cube-creator#Main'), debug('test'))
+      const runner = publish($rdf.namedNode('urn:pipeline:cube-creator#Main'), debug('test'))
 
-    // when
-    await runner({
-      debug: true,
-      job: `${env.API_CORE_BASE}cube-project/ubd/csv-mapping/jobs/test-publish-job`,
-      executionUrl,
+      // when
+      await runner({
+        debug: true,
+        job: `${env.API_CORE_BASE}cube-project/ubd/csv-mapping/jobs/test-publish-job`,
+        executionUrl,
+      })
+      await new Promise((resolve) =>
+        setTimeout(resolve, 100),
+      )
+
+      // then
+      const expectedGraph = ns.targetCube()
+      const dataset = await $rdf.dataset().import(await CONSTRUCT`?s ?p ?o`
+        .FROM(expectedGraph)
+        .WHERE`?s ?p ?o`
+        .execute(client.query))
+
+      cubePointer = clownface({ dataset })
     })
-    await new Promise((resolve) =>
-      setTimeout(resolve, 100),
-    )
 
-    // then
-    const expectedGraph = ns.targetCube()
-    const dataset = await $rdf.dataset().import(await CONSTRUCT`?s ?p ?o`
-      .FROM(expectedGraph)
-      .WHERE`?s ?p ?o`
-      .execute(client.query))
+    it('cube meta data has been copied', async function () {
+      
 
-    const cubePointer = clownface({ dataset })
-    expect(cubePointer.has(rdf.type, sh.NodeShape).terms.length).to.eq(1)
+      expect(cubePointer.namedNode(ns.targetCube())).to.matchShape({
+        property: [{
+          path: dcterms.identifier,
+          hasValue: $rdf.literal('UBD28@BAFU'),
+          minCount: 1,
+          maxCount: 1,
+        }, {
+          path: dcterms.title,
+          hasValue: $rdf.literal('UBD28'),
+          minCount: 1,
+          maxCount: 1,
+        }, {
+          path: dcat.contactPoint,
+          minCount: 1,
+          maxCount: 1,
 
-    expect(cubePointer).to.matchShape({
-      property: [{
-        path: dcterms.identifier,
-        hasValue: $rdf.literal('UBD28@BAFU'),
-        minCount: 1,
-        maxCount: 1,
-      }, {
-        path: dcterms.title,
-        hasValue: $rdf.literal('UBD28'),
-        minCount: 1,
-        maxCount: 1,
-      }, {
-        path: dcat.contactPoint,
-        minCount: 1,
-        maxCount: 1,
-        node: {
-          property: [{
-            path: vcard.fn,
-            hasValue: $rdf.literal('Test Name'),
-            minCount: 1,
-          },
-          {
-            path: vcard.fn,
-            hasValue: $rdf.literal('test@mail.ch'),
-            minCount: 1,
-          }],
+        }],
+      })
+
+      expect(cubePointer.namedNode(ns.targetCube()).out(dcat.contactPoint)).to.matchShape({
+        property: [{
+          path: vcard.fn,
+          hasValue: $rdf.literal('Test Name'),
+          minCount: 1,
         },
-      }],
+        {
+          path: vcard.hasEmail,
+          hasValue: $rdf.literal('test@mail.ch'),
+          minCount: 1,
+        }],
+      })
     })
 
-    expect(cubePointer.namedNode(ns.targetCube('observation/blBAS-2002-annualmean'))).to.matchShape({
-      property: [{
-        path: rdf.type,
-        hasValue: cube.Observation,
-        minCount: 1,
-      }, {
-        path: cube.observedBy,
-        minCount: 1,
-      }, {
-        path: ns.targetCube('dimension/year'),
-        hasValue: $rdf.literal('2002', xsd.gYear),
-        minCount: 1,
-        maxCount: 1,
-      }, {
-        path: ns.targetCube('dimension/value'),
-        hasValue: $rdf.literal('6.1', xsd.float),
-        minCount: 1,
-        maxCount: 1,
-      }, {
-        path: ns.targetCube('station'),
-        minCount: 1,
-        maxCount: 1,
-      }, {
-        path: ns.targetCube('pollutant'),
-        minCount: 1,
-        maxCount: 1,
-      }, {
-        path: ns.targetCube('unit-id'),
-        hasValue: $rdf.literal('µg/m3'),
-        minCount: 1,
-        maxCount: 1,
-      }],
+    it('observation data has been copied', async function () {
+      expect(cubePointer.namedNode(ns.targetCube('observation/blBAS-2002-annualmean'))).to.matchShape({
+        property: [{
+          path: rdf.type,
+          hasValue: cube.Observation,
+          minCount: 1,
+        }, {
+          path: cube.observedBy,
+          minCount: 1,
+        }, {
+          path: ns.targetCube('dimension/year'),
+          hasValue: $rdf.literal('2002', xsd.gYear),
+          minCount: 1,
+          maxCount: 1,
+        }, {
+          path: ns.targetCube('dimension/value'),
+          hasValue: $rdf.literal('6.1', xsd.float),
+          minCount: 1,
+          maxCount: 1,
+        }, {
+          path: ns.targetCube('station'),
+          minCount: 1,
+          maxCount: 1,
+        }, {
+          path: ns.targetCube('pollutant'),
+          minCount: 1,
+          maxCount: 1,
+        }, {
+          path: ns.targetCube('unit-id'),
+          hasValue: $rdf.literal('µg/m3'),
+          minCount: 1,
+          maxCount: 1,
+        }],
+      })
     })
 
-    expect(cubePointer.namedNode(ns.targetCube('shape/'))).to.matchShape({
-      property: {
-        path: rdf.type,
-        hasValue: cube.Constraint,
-        minCount: 1,
-      },
-    })
+    it('dimension meta data has been copied', async function () {
+      expect(cubePointer.has(rdf.type, sh.NodeShape).term).to.deep.eq(ns.targetCube('shape/'))
+      expect(cubePointer.has(rdf.type, sh.NodeShape).terms.length).to.eq(1)
+      expect(cubePointer.namedNode(ns.targetCube('shape/'))).to.matchShape({
+        property: {
+          path: rdf.type,
+          hasValue: cube.Constraint,
+          minCount: 1,
+        },
+      })
 
-    expect(cubePointer.namedNode(ns.targetCube('shape/')).out(sh.property).has(sh.path, ns.targetCube('dimension/year'))).to.matchShape({
-      property: {
-        path: sh.path,
-        hasValue: ns.targetCube('dimension/year'),
-        minCount: 1,
-      },
-    })
+      expect(cubePointer.namedNode(ns.targetCube('shape/')).out(sh.property).has(sh.path, ns.targetCube('dimension/year'))).to.matchShape({
+        property: {
+          path: sh.path,
+          hasValue: ns.targetCube('dimension/year'),
+          minCount: 1,
+        },
+      })
 
-    expect(cubePointer.namedNode(ns.targetCube('shape/')).out(sh.property).has(sh.path, schema.name)).to.matchShape({
-      property: {
-        path: schema.name,
-        hasValue: $rdf.literal('Jahr', 'de'),
-        minCount: 1,
-      },
-    })
+      expect(cubePointer.namedNode(ns.targetCube('shape/')).out(sh.property).has(sh.path, schema.name)).to.matchShape({
+        property: {
+          path: schema.name,
+          hasValue: $rdf.literal('Jahr', 'de'),
+          minCount: 1,
+        },
+      })
 
-    expect(cubePointer.namedNode(ns.targetCube('shape/')).out(sh.property).has(sh.path, scale.scaleOfMeasure)).to.matchShape({
-      property: {
-        path: scale.scaleOfMeasure,
-        hasValue: scale.Temporal,
-        minCount: 1,
-      },
+      expect(cubePointer.namedNode(ns.targetCube('shape/')).out(sh.property).has(sh.path, scale.scaleOfMeasure)).to.matchShape({
+        property: {
+          path: scale.scaleOfMeasure,
+          hasValue: scale.Temporal,
+          minCount: 1,
+        },
+      })
     })
   })
 })
