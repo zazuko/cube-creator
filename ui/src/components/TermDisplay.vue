fbbf9a23bf663f5acd1bb6c762ee268ee521f51e
Martin Maillard <self@martin-maillard.com>
2020-12-08 11:38:18 +0100

    fix(ui): reference display in cube preview

--- ui/src/components/TermDisplay.vue
+++ ui/src/components/TermDisplay.vue
@@ -4,10 +4,13 @@
   </span>
   <span v-else-if="term.termType === 'Literal'">
     {{ term.value }}
-    <span v-if="term.language" class="is-small">{{ term.language }}</span>
+    <span v-if="term.language" class="is-small has-text-grey">@{{ term.language }}</span>
   </span>
   <span v-else-if="term.termType === 'NamedNode'">
     {{ shrink(term.value) }}
+  </span>
+  <span v-else-if="term.id" class="tag is-rounded is-small">
+    {{ shrink(term.id.value) }}
   </span>
   <span v-else>
     {{ term.value }}
@@ -17,11 +20,12 @@
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator'
 import { Term } from 'rdf-js'
+import RdfResource from '@tpluscode/rdfine/RdfResource'
 import { shrink } from '@/rdf-properties'
 
 @Component
 export default class extends Vue {
-  @Prop() term?: Term[]
+  @Prop() term?: Term | RdfResource
   @Prop({ default: '' }) missingValue?: string
 
   shrink (value: string): string {
