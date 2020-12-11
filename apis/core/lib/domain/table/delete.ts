b726a73e66c270d2339557b2ec1d3674c0952f84
Luca Furrer <luca.s.furrer@gmail.com>
2020-12-11 08:13:19 +0100

    fix: delete dimensions when deleting table

--- apis/core/lib/domain/table/delete.ts
+++ apis/core/lib/domain/table/delete.ts
@@ -1,33 +1,41 @@
-import { NamedNode, Term } from 'rdf-js'
+import { NamedNode } from 'rdf-js'
 import { ResourceStore } from '../../ResourceStore'
 import { cc } from '@cube-creator/core/namespace'
+import * as DimensionMetadataQueries from '../queries/dimension-metadata'
+import * as TableQueries from '../queries/table'
+import { deleteColumnMapping } from '../column-mapping/delete'
 
 interface DeleteTableCommand {
   resource: NamedNode
   store: ResourceStore
+  dimensionMetadataQueries?: Pick<typeof DimensionMetadataQueries, 'getDimensionMetaDataCollection'>
+  tableQueries?: Pick<typeof TableQueries, 'getTableForColumnMapping'>
 }
 
 export async function deleteTable({
-  resource: tableTerm,
+  resource: tableTerm,,
   store,
+  dimensionMetadataQueries: { getDimensionMetaDataCollection } = DimensionMetadataQueries,
+  tableQueries: { getTableForColumnMapping } = TableQueries,
 }: DeleteTableCommand): Promise<void> {
   if (tableTerm.termType !== 'NamedNode') return
-
+  
   const table = await store.get(tableTerm, { allowMissing: true })
   if (!table) return
 
   // Delete in columnMappings
   const columnMappings = table.out(cc.columnMapping).terms
   for await (const columnMapping of columnMappings) {
-    await deleteColumnMapping(columnMapping, store)
+    if (columnMapping.termType === 'NamedNode') {
+      await deleteColumnMapping({
+        resource: columnMapping,
+        store,
+        dimensionMetadataQueries: { getDimensionMetaDataCollection },
+        tableQueries: { getTableForColumnMapping },
+      })
+    }
   }
 
   // Delete Graph
   store.delete(tableTerm)
-}
-
-async function deleteColumnMapping(columnMapping: Term, store: ResourceStore) {
-  if (columnMapping?.termType === 'NamedNode') {
-    await store.delete(columnMapping)
-  }
 }
