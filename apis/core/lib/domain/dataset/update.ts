74f897efb419639beffcbc62f03efb46eb4fca1c
tpluscode <tpluscode@users.noreply.github.com>
2020-12-16 16:24:54 +0100

    fix: more selectively accept dataset metadata

--- apis/core/lib/domain/dataset/update.ts
+++ apis/core/lib/domain/dataset/update.ts
@@ -1,14 +1,31 @@
 import { cc } from '@cube-creator/core/namespace'
 import { dcat, hydra, rdf, schema, _void } from '@tpluscode/rdf-ns-builders'
 import { GraphPointer } from 'clownface'
-import { NamedNode } from 'rdf-js'
+import { DatasetCore, NamedNode, Term } from 'rdf-js'
+import TermSet from '@rdfjs/term-set'
 import { ResourceStore } from '../../ResourceStore'
 
 interface AddMetaDataCommand {
   dataset: GraphPointer<NamedNode>
   resource: GraphPointer
   store: ResourceStore
+}
 
+/**
+ * Copies quads of given subject and its blank node children
+ */
+function copySubgraph({ from, to, subject, alreadyAdded = new TermSet() }: { from: DatasetCore; to: DatasetCore; subject: Term; alreadyAdded?: TermSet }) {
+  if (alreadyAdded.has(subject)) {
+    return
+  }
+  alreadyAdded.add(subject)
+
+  for (const quad of from.match(subject)) {
+    to.add(quad)
+    if (quad.object.termType === 'BlankNode') {
+      copySubgraph({ from, to, alreadyAdded, subject: quad.object })
+    }
+  }
 }
 
 export async function update({
@@ -28,9 +45,11 @@ export async function update({
   })
   datasetResource.deleteOut()
 
-  for (const quad of resource.dataset) {
-    datasetResource.dataset.add(quad)
-  }
+  copySubgraph({
+    from: resource.dataset,
+    to: datasetResource.dataset,
+    subject: datasetResource.term,
+  })
 
   // Make sure the type is correct
   datasetResource.addOut(rdf.type, hydra.Resource)
