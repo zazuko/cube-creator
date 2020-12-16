17f61b6dab667150f879e3cdbd20dc9e8dc7bf97
tpluscode <tpluscode@users.noreply.github.com>
2020-12-16 08:15:33 +0100

    fix: serve csv files directly from s3

--- cli/lib/csv.ts
+++ cli/lib/csv.ts
@@ -6,21 +6,32 @@ import { Stream } from 'rdf-js'
 import * as Csvw from '@rdfine/csvw'
 import { Hydra } from 'alcaeus/node'
 import { readable } from 'duplex-to'
+import { CsvSource } from '@cube-creator/model'
+import fetch from 'node-fetch'
 
 export function openFromCsvw(csvw: Csvw.Table) {
   const csvStream = new PassThrough()
 
   Promise.resolve().then(async () => {
-    const { response } = await Hydra.loadResource(csvw.url!, {
-      accept: 'text/csv',
-    })
-
-    if (!response?.xhr.ok || !response.xhr.body) {
-      csvStream.emit('error', new Error('Failed to load CSV. Response was: ' + response?.xhr.statusText))
+    const { response, representation } = await Hydra.loadResource<CsvSource>(csvw.url!)
+    if (!representation?.root) {
+      csvStream.emit('error', new Error('Failed to load Source. Response was: ' + response?.xhr.statusText))
       return
     }
 
-    (response.xhr.body as any).pipe(csvStream)
+    if (!representation.root.associatedMedia.contentUrl) {
+      csvStream.emit('error', new Error('Failed to load CSV. Missing link from source'))
+      return
+    }
+
+    const csvResponse = await fetch(representation.root.associatedMedia.contentUrl.value)
+
+    if (!csvResponse?.ok || !csvResponse.body) {
+      csvStream.emit('error', new Error('Failed to load CSV. Response was: ' + csvResponse?.statusText))
+      return
+    }
+
+    (csvResponse.body as any).pipe(csvStream)
   })
 
   return readable(csvStream)
