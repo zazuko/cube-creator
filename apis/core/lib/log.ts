94f3091e18a4b166c913e0879359361c7ca19513
tpluscode <tpluscode@users.noreply.github.com>
2020-12-16 13:58:18 +0100

    fix: allow more error logging in Sentry

--- apis/core/lib/log.ts
+++ apis/core/lib/log.ts
@@ -1,5 +1,8 @@
 import debug from 'debug'
 
+// eslint-disable-next-line no-console
+debug.log = console.log.bind(console)
+
 export const log = debug('creator')
 export const warning = log.extend('warning')
 export const error = log.extend('error')
