355dbd79325dee3f88bc86a879adab16d9ed0898
Martin Maillard <self@martin-maillard.com>
2020-12-22 17:46:50 +0100

    chore: include linked resources in table PUT/POST

--- apis/core/lib/handlers/table/create.ts
+++ apis/core/lib/handlers/table/create.ts
@@ -1,8 +1,12 @@
 import asyncMiddleware from 'middleware-async'
 import { protectedResource } from '@hydrofoil/labyrinth/resource'
+import { loadLinkedResources } from '@hydrofoil/labyrinth/lib/query/eagerLinks'
 import { shaclValidate } from '../../middleware/shacl'
 import { createTable } from '../../domain/table/create'
+import { query } from '@cube-creator/core/namespace'
 import clownface from 'clownface'
+import $rdf from 'rdf-ext'
+import { rdf } from '@tpluscode/rdf-ns-builders'
 
 export const post = protectedResource(
   shaclValidate,
@@ -16,6 +20,13 @@ export const post = protectedResource(
 
     res.status(201)
     res.header('Location', table.value)
-    await res.dataset(table.dataset)
+
+    // Include resources defined with `query:include`
+    const types = clownface({
+      dataset: req.hydra.api.dataset,
+      term: table.out(rdf.type).terms,
+    })
+    const linkedResources = await loadLinkedResources(table, types.out(query.include).toArray(), req.app.sparql)
+    await res.dataset($rdf.dataset([...table.dataset, ...linkedResources]))
   }),
 )
