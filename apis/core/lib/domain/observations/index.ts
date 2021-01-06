72d834ec21336f4bfd08e0bf7026ef734b654fab
Martin Maillard <self@martin-maillard.com>
2021-01-06 11:26:01 +0100

    chore: fix tests

--- apis/core/lib/domain/observations/index.ts
+++ apis/core/lib/domain/observations/index.ts
@@ -4,7 +4,7 @@ import { createSource, createView, createHydraCollection, populateFilters } from
 import { DomainError } from '../../errors'
 import { rdf } from '@tpluscode/rdf-ns-builders'
 import * as ns from '@cube-creator/core/namespace'
-import { loadResourceLabels } from '../queries/observations'
+import { loadResourceLabels as _loadResourceLabels } from '../queries/observations'
 import { Quad, Term } from 'rdf-js'
 
 const DEFAULT_PAGE_SIZE = 20
@@ -16,9 +16,18 @@ interface Params {
   template: IriTemplate
   filters?: AnyPointer
   pageSize?: number
+  loadResourceLabels?: (ids: Term[]) => Promise<Quad[]>
 }
 
-export async function getObservations({ cubeId, sourceGraph, filters, templateParams, template, pageSize }: Params): Promise<Collection> {
+export async function getObservations({
+  cubeId,
+  sourceGraph,
+  filters,
+  templateParams,
+  template,
+  pageSize,
+  loadResourceLabels = _loadResourceLabels,
+}: Params): Promise<Collection> {
   const source = createSource(sourceGraph)
 
   const cubes = await source.cubes()
@@ -42,13 +51,13 @@ export async function getObservations({ cubeId, sourceGraph, filters, templatePa
   })
 
   // Load labels for linked resources
-  const labelQuads = await getLabels(observations)
+  const labelQuads = await getLabels(observations, loadResourceLabels)
   labelQuads.forEach((quad) => collection.pointer.dataset.add(quad))
 
   return collection
 }
 
-async function getLabels (observations: Record<string, Term>[]): Promise<Quad[]> {
+async function getLabels(observations: Record<string, Term>[], loadResourceLabels: (ids: Term[]) => Promise<Quad[]>): Promise<Quad[]> {
   const blackList = [rdf.type.value, ns.cube.observedBy.value]
   const resourceIds = new Set(observations.flatMap((observation) => {
     return Object.entries(observation)
