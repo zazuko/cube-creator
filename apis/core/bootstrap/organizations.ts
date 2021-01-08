c39fba58cdd9162e3df9b8718576272164255932
tpluscode <tpluscode@users.noreply.github.com>
2021-01-08 15:30:02 +0100

    refactor: make organization part of default data insert

--- apis/core/bootstrap/organizations.ts
+++ apis/core/bootstrap/organizations.ts
@@ -1,5 +1,6 @@
 import { turtle } from '@tpluscode/rdf-string'
-import { hydra, rdf, schema } from '@tpluscode/rdf-ns-builders'
+import { hydra, rdf, rdfs, schema } from '@tpluscode/rdf-ns-builders'
+import { cc } from '@cube-creator/core/namespace'
 
 export const organizations = turtle`
 <organizations> {
@@ -9,5 +10,17 @@ export const organizations = turtle`
       ${hydra.property} ${rdf.type} ;
       ${hydra.object} ${schema.Organization}
     ] .
+}
+
+<organization/bafu> {
+  <organization/bafu>
+    a ${schema.Organization} ;
+    ${cc.publishGraph}  <https://lindas.admin.ch/foen/cube> ;
+    ${cc.namespace} <https://environment.ld.admin.ch/foen/> ;
+    ${rdfs.label} "Bundesamt für Umwelt BAFU"@de ;
+    ${rdfs.label} "Office fédéral de l'environnement OFEV"@fr ;
+    ${rdfs.label} "Ufficio federale dell'ambiente UFAM"@it ;
+    ${rdfs.label} "Federal Office for the Environment FOEN"@en ;
+  .
 }
 `
