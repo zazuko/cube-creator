3b8df5a6e8d3e3fac64e1bacd52f605335c33548
Martin Maillard <self@martin-maillard.com>
2021-01-11 17:12:14 +0100

    chore: fix e2e tests

--- ui/tests/e2e/specs/project-mapping.spec.ts
+++ ui/tests/e2e/specs/project-mapping.spec.ts
@@ -25,6 +25,10 @@ describe('CSV mapping flow', () => {
       .find('select')
       .select('https://cube-creator.lndo.site/organization/bafu')
 
+    // Click somewhere to validate the organization selection
+    cy.contains('.form-property', 'Organization')
+      .click()
+
     cy.get('form').submit()
 
     cy.contains('successfully created').should('be.visible')
