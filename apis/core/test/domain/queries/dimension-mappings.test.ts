bb194f6d6e05ef80d59f8866bb4208242dfc9503
tpluscode <tpluscode@users.noreply.github.com>
2021-01-27 18:17:30 +0100

    feat: added domain code to set dimension mappings

---
+++ apis/core/test/domain/queries/dimension-mappings.test.ts
@@ -0,0 +1,54 @@
+import { describe, it, beforeEach } from 'mocha'
+import sinon from 'sinon'
+import { Term } from 'rdf-js'
+import $rdf from 'rdf-ext'
+import { expect } from 'chai'
+import { DESCRIBE } from '@tpluscode/sparql-builder'
+import { parsingClient, client, insertTestData } from '@cube-creator/testing/lib'
+import clownface from 'clownface'
+import { replaceValueWithDefinedTerms } from '../../../lib/domain/queries/dimension-mappings'
+
+describe('lib/domain/queries/dimension-mappings @SPARQL', function () {
+  this.timeout(20000)
+
+  const dimensionMapping = $rdf.namedNode('https://cube-creator.lndo.site/cube-project/ubd/dimension-mapping/pollutant')
+  let terms: Map<string, Term>
+  const sparqlSpy = sinon.spy(client.query, 'update')
+
+  beforeEach(async () => {
+    terms = new Map()
+    await insertTestData('fuseki/sample-ubd.trig')
+
+    sparqlSpy.resetHistory()
+  })
+
+  it('does nothing when called without any terms', async () => {
+    // when
+    await replaceValueWithDefinedTerms({ dimensionMapping, terms }, client)
+
+    // then
+    expect(sparqlSpy).not.to.have.been.called
+  })
+
+  it('updates observations', async () => {
+    // given
+    const observationId = $rdf.namedNode('https://environment.ld.admin.ch/foen/ubd/28/observation/blBAS-2003-annualmean')
+    const definedTerm = $rdf.namedNode('http://www.wikidata.org/entity/Q5282')
+    terms.set('so2', definedTerm)
+
+    // when
+    await replaceValueWithDefinedTerms({ dimensionMapping, terms }, client)
+
+    // then
+    const quads = await DESCRIBE`${observationId}`.execute(parsingClient.query)
+    const observation = clownface({ dataset: $rdf.dataset(quads) }).namedNode(observationId)
+    expect(observation).to.matchShape({
+      property: {
+        path: $rdf.namedNode('https://environment.ld.admin.ch/foen/ubd/28/pollutant'),
+        hasValue: definedTerm,
+        minCount: 1,
+        maxCount: 1,
+      },
+    })
+  })
+})
