317daa27733083772e7ac060614756445a7d1264
tpluscode <tpluscode@users.noreply.github.com>
2021-01-28 10:42:16 +0100

    chore: rebased, now update code

--- apis/core/lib/domain/dimension/DimensionMetadata.ts
+++ apis/core/lib/domain/dimension/DimensionMetadata.ts
@@ -1,8 +1,8 @@
-import { scale } from '@cube-creator/core/namespace'
+import { qudt } from '@tpluscode/rdf-ns-builders'
 import type{ DimensionMetadata } from '@cube-creator/model'
 
 export function canBeMappedToManagedDimension(dimension: DimensionMetadata): boolean {
   const { scaleOfMeasure } = dimension
 
-  return !!scaleOfMeasure && (scaleOfMeasure.equals(scale.Nominal) || scaleOfMeasure.equals(scale.Ordinal))
+  return !!scaleOfMeasure && (scaleOfMeasure.equals(qudt.NominalScale) || scaleOfMeasure.equals(qudt.OrdinalScale))
 }
