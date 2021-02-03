7550f3d6537d2ec535b488c185f08cb01cc36515
Luca Furrer <luca.s.furrer@gmail.com>
2021-02-03 18:23:07 +0100

    refactor: allow not async barnard map

--- typings/barnard59-base/index.d.ts
+++ typings/barnard59-base/index.d.ts
@@ -1,7 +1,7 @@
 declare module 'barnard59-base/lib/map' {
   import * as stream from 'stream'
 
-  function map<T>(cb: (chunk: T, encoding: string) => Promise<T>): stream.Transform
+  function map<T>(cb: (chunk: T, encoding: string) => Promise<T> | T): stream.Transform
 
   export = map
 }
