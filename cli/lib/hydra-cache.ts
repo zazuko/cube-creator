1f0067a697f7720cf4d3401ff9efba5abf1b65bb
tpluscode <tpluscode@users.noreply.github.com>
2021-02-10 21:01:09 +0100

    perf(cli): improve mappings step of transformation pipeline

--- cli/lib/hydra-cache.ts
+++ cli/lib/hydra-cache.ts
@@ -1,7 +1,9 @@
 import { Hydra } from 'alcaeus/node'
 import { hydra, prov } from '@tpluscode/rdf-ns-builders'
+import { cc } from '@cube-creator/core/namespace'
 
 Hydra.cacheStrategy.shouldLoad = (previous) => {
   return !previous.representation.root?.types.has(prov.Dictionary) ||
-  !previous.representation.root?.types.has(hydra.ApiDocumentation)
+  !previous.representation.root?.types.has(hydra.ApiDocumentation) ||
+  !previous.representation.root?.types.has(cc.Job)
 }
