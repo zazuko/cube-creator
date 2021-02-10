31635f28fae9d6299cf6ca978a4f35a0791b124f
tpluscode <tpluscode@users.noreply.github.com>
2021-02-10 20:41:26 +0100

    build(dep): update barnard59-formats

--- typings/barnard59-base/index.d.ts
+++ typings/barnard59-base/index.d.ts
@@ -1,7 +1,8 @@
 declare module 'barnard59-base/lib/map' {
   import * as stream from 'stream'
+  import type { Context } from 'barnard59-core/lib/Pipeline'
 
-  function map<T>(cb: (chunk: T, encoding: string) => Promise<T> | T): stream.Transform
+  function map<T>(cb: (this: Context, chunk: T, encoding: string) => Promise<T> | T): stream.Transform
 
   export = map
 }
