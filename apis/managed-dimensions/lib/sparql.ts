33a198042c26fb946ef0e09566809a1515e4b8c9
tpluscode <tpluscode@users.noreply.github.com>
2021-02-17 15:30:58 +0100

    chore: consistent sparql endpoint variables

--- apis/managed-dimensions/lib/sparql.ts
+++ apis/managed-dimensions/lib/sparql.ts
@@ -1,7 +1,7 @@
 import env from './env'
 
 export const sparql = {
-  endpointUrl: env.MANAGED_DIMENSIONS_STORE_ENDPOINT,
+  endpointUrl: env.MANAGED_DIMENSIONS_STORE_QUERY_ENDPOINT,
   updateUrl: env.MANAGED_DIMENSIONS_STORE_UPDATE_ENDPOINT,
   storeUrl: env.MANAGED_DIMENSIONS_STORE_GRAPH_ENDPOINT,
   user: env.maybe.MANAGED_DIMENSIONS_STORE_USERNAME,
