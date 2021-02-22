ff42912ac07af5613e8cccd38c8ead327a533359
Martin Maillard <self@martin-maillard.com>
2021-02-22 15:05:20 +0100

    feat(ui): improve form validation messages

--- ui/src/components/HydraOperationError.vue
+++ ui/src/components/HydraOperationError.vue
@@ -1,13 +1,14 @@
 <template>
   <b-message v-if="error" type="is-danger" :title="error.title" class="error-message">
-    <div class="content">
-      <p v-if="error.detail">
+    <div class="">
+      <p v-if="error.detail && !error.report">
         {{ error.detail }}
       </p>
-      <ul v-if="error.report">
-        <li v-for="(reportError, reportIndex) in error.report" :key="reportIndex">
-          {{ shrink(reportError.path) }}
-          <ul>
+      <ul v-if="error.report" class="">
+        <li v-for="(reportError, reportIndex) in error.report" :key="reportIndex" class="mb-2">
+          <strong>{{ propertyLabel(reportError.path) }}</strong>:&nbsp;
+          <span v-if="reportError.message.length === 1">{{ reportError.message[0] }}</span>
+          <ul v-else>
             <li v-for="(message, messageIndex) in reportError.message" :key="messageIndex">
               {{ message }}
             </li>
@@ -20,14 +21,29 @@
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator'
+import type { Shape } from '@rdfine/shacl'
+import $rdf from '@rdf-esm/data-model'
 import { ErrorDetails } from '@/api/errors'
+import { sh } from '@tpluscode/rdf-ns-builders'
 
 @Component
 export default class extends Vue {
   @Prop({ default: null }) error!: ErrorDetails | null
+  @Prop({ default: null }) shape!: Shape | null
 
-  shrink (uri: string | undefined): string {
-    return uri ? uri.split('#').slice(-1)[0] : ''
+  propertyLabel (uri: string | undefined): string {
+    if (!uri) return ''
+
+    const shrunkProperty = uri.split('#').slice(-1)[0]
+
+    if (!this.shape) return shrunkProperty
+
+    const label = this.shape.pointer.any()
+      .has(sh.path, $rdf.namedNode(uri))
+      .out(sh.name, { language: ['en', ''] })
+      .value
+
+    return label || shrunkProperty
   }
 }
 </script>
