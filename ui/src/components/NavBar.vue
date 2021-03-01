8726055e1ead5a9b5574bf3b0a71f07b41ffa7ad
Martin Maillard <self@martin-maillard.com>
2021-03-01 10:24:49 +0100

    feat(ui): add link to documentation wiki

--- ui/src/components/NavBar.vue
+++ ui/src/components/NavBar.vue
@@ -11,6 +11,14 @@
       </b-navbar-item>
     </template>
     <template slot="end">
+      <b-navbar-item
+        href="https://github.com/zazuko/cube-creator/wiki"
+        target="_blank"
+        rel="noopener noreferrer nofollow"
+      >
+        <b-icon icon="question-circle" />
+        <span>Help</span>
+      </b-navbar-item>
       <b-navbar-item>
         <sign-out-button />
       </b-navbar-item>
