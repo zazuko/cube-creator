5d509c75948b97eb17d4dd5f2be8ab8fe168630c
Martin Maillard <self@martin-maillard.com>
2021-03-01 16:54:10 +0100

    feat(ui): create managed dimension term

--- ui/src/views/ManagedDimension.vue
+++ ui/src/views/ManagedDimension.vue
@@ -1,9 +1,48 @@
 <template>
   <page-content>
     <div v-if="dimension">
-      <h2 class="title is-size-4 mb-0">
-        <term-with-language :values="dimension.name" :selected-language="language" />
-      </h2>
+      <div class="mb-4">
+        <h2 class="title is-size-4 mb-0">
+          <term-with-language :values="dimension.name" :selected-language="language" />
+        </h2>
+        <hydra-operation-button
+          :operation="dimension.actions.edit"
+          :to="{ name: 'ManagedDimensionEdit' }"
+          type="is-default"
+          size=""
+        />
+      </div>
+      <table class="table is-narrow is-fullwidth">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Identifiers</th>
+            <th>
+              <hydra-operation-button
+                v-if="dimension.actions.create"
+                :operation="dimension.actions.create"
+                :to="{ name: 'ManagedTermCreate' }"
+                type="is-default"
+                size=""
+              >
+                {{ dimension.actions.create.title }}
+              </hydra-operation-button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td colspan="3">
+              No term in this dimension yet. Do you want to
+              <router-link :to="{ name: 'ManagedTermCreate' }">
+                create one
+              </router-link>?
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <router-view />
     </div>
     <loading-block v-else />
   </page-content>
@@ -12,6 +51,7 @@
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 import { namespace } from 'vuex-class'
+import HydraOperationButton from '@/components/HydraOperationButton.vue'
 import LoadingBlock from '@/components/LoadingBlock.vue'
 import PageContent from '@/components/PageContent.vue'
 import TermWithLanguage from '@/components/TermWithLanguage.vue'
@@ -24,7 +64,7 @@ interface ManagedDimension {
 }
 
 @Component({
-  components: { LoadingBlock, PageContent, TermWithLanguage },
+  components: { HydraOperationButton, LoadingBlock, PageContent, TermWithLanguage },
 })
 export default class extends Vue {
   @appNS.State('language') language!: string
