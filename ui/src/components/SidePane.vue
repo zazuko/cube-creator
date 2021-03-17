44782a8d89b4acfc0f0d132469bdb89f9253bfa8
Martin Maillard <self@martin-maillard.com>
2021-03-17 12:04:08 +0100

    feat(ui): make side-panes resizable

--- ui/src/components/SidePane.vue
+++ ui/src/components/SidePane.vue
@@ -1,30 +1,71 @@
 <template>
-  <div class="quickview" :class="{ 'is-active': isOpen }">
-    <header class="quickview-header">
-      <h2>{{ title }}</h2>
-      <button class="delete" @click="$emit('close')" />
-    </header>
+  <div class="quickview" :class="{ 'is-active': isOpen }" :style="width ? { width: width } : {}">
+    <div @mousedown="onMouseDown" class="handle" />
+    <div class="quickview-inner">
+      <header class="quickview-header">
+        <h2>{{ title }}</h2>
+        <button class="delete" @click="$emit('close')" />
+      </header>
 
-    <div class="quickview-body">
-      <slot />
+      <div class="quickview-body">
+        <slot />
+      </div>
+      <!-- <footer class="quickview-footer"></footer> -->
     </div>
-    <!-- <footer class="quickview-footer"></footer> -->
   </div>
 </template>
 
 <script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
 import 'bulma-quickview/dist/css/bulma-quickview.min.css'
 
-export default {
-  name: 'SidePane',
-  props: {
-    isOpen: Boolean,
-    title: { type: String, required: true },
-  },
+@Component({})
+export default class SidePane extends Vue {
+  @Prop({ default: true }) isOpen!: boolean
+  @Prop({ required: true }) title!: string
+
+  width: string | null = null
+
+  onMouseDown (): void {
+    const containerWidth = window.innerWidth
+
+    const onMouseMove = ({ pageX }: MouseEvent) => {
+      this.width = `${containerWidth - pageX}px`
+    }
+
+    const onMouseUp = () => {
+      window.removeEventListener('mousemove', onMouseMove)
+      window.removeEventListener('mouseup', onMouseUp)
+    }
+
+    window.addEventListener('mousemove', onMouseMove)
+    window.addEventListener('mouseup', onMouseUp)
+  }
 }
 </script>
 
 <style scoped>
+.quickview {
+  transition: none;
+
+  flex-direction: row;
+}
+
+.handle {
+  width: 3px;
+  margin-right: -3px;
+  cursor: col-resize;
+  z-index: 10;
+}
+
+.quickview-inner {
+  flex-grow: 1;
+  overflow: auto;
+
+  display: flex;
+  flex-direction: column;
+}
+
 .quickview-body {
   padding: 1rem;
 }
