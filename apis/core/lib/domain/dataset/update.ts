09f01600b32f4e1d690e7d78a1c3c2e06717a6f8
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2021-03-26 13:10:55 +0100

    fix: clean cube meta graph on save

--- apis/core/lib/domain/dataset/update.ts
+++ apis/core/lib/domain/dataset/update.ts
@@ -1,31 +1,13 @@
 import { cc } from '@cube-creator/core/namespace'
 import { dcat, hydra, rdf, schema, _void } from '@tpluscode/rdf-ns-builders'
 import { GraphPointer } from 'clownface'
-import { DatasetCore, NamedNode, Term } from 'rdf-js'
-import TermSet from '@rdfjs/term-set'
+import { NamedNode } from 'rdf-js'
 import { ResourceStore } from '../../ResourceStore'
 
 interface AddMetaDataCommand {
   dataset: GraphPointer<NamedNode>
   resource: GraphPointer
   store: ResourceStore
-}
-
-/**
- * Copies quads of given subject and its blank node children
- */
-function copySubgraph({ from, to, subject, alreadyAdded = new TermSet() }: { from: DatasetCore; to: DatasetCore; subject: Term; alreadyAdded?: TermSet }) {
-  if (alreadyAdded.has(subject)) {
-    return
-  }
-  alreadyAdded.add(subject)
-
-  for (const quad of from.match(subject)) {
-    to.add(quad)
-    if (quad.object.termType === 'BlankNode') {
-      copySubgraph({ from, to, alreadyAdded, subject: quad.object })
-    }
-  }
 }
 
 export async function update({
@@ -38,18 +20,12 @@ export async function update({
   const hasPart = datasetResource.out(schema.hasPart)
   const dimensionMetadata = datasetResource.out(cc.dimensionMetadata)
 
-  datasetResource.out().forEach(child => {
-    if (child.term.termType === 'BlankNode') {
-      child.deleteOut()
-    }
-  })
-  datasetResource.deleteOut()
-
-  copySubgraph({
-    from: resource.dataset,
-    to: datasetResource.dataset,
-    subject: datasetResource.term,
-  })
+  for (const quad of datasetResource.dataset) {
+    datasetResource.dataset.delete(quad)
+  }
+  for (const quad of resource.dataset) {
+    datasetResource.dataset.add(quad)
+  }
 
   // Make sure the type is correct
   datasetResource.addOut(rdf.type, hydra.Resource)
