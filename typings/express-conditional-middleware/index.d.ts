67f37d65d57f98047b2a712efc553d5b6290497e
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2021-09-09 15:27:58 +0200

    fix: build type errors

--- typings/express-conditional-middleware/index.d.ts
+++ typings/express-conditional-middleware/index.d.ts
@@ -2,7 +2,7 @@ declare module 'express-conditional-middleware' {
   import type * as E from 'express'
 
   function conditional(
-    condition: boolean | ((req: E.Request, res:E.Response, next: E.NextFunction) => boolean),
+    condition: boolean | ((req: E.Request, res:E.Response, next: E.NextFunction) => boolean | undefined),
     onTrue: E.RequestHandler,
     onFalse?: E.RequestHandler
   ): E.Router
--- ui/src/forms/FileLiteral.ts
+++ ui/src/forms/FileLiteral.ts
@@ -1,4 +1,8 @@
+import { Literal } from 'rdf-js'
 import LiteralExt from 'rdf-ext/lib/Literal'
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface FileLiteral extends Literal {}
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
