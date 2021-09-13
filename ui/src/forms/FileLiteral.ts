acf6b430aba5cca502bd54c8452a192e1bae3c41
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2021-09-13 09:21:33 +0200

    build(deps): rdf-ext types

--- ui/src/forms/FileLiteral.ts
+++ ui/src/forms/FileLiteral.ts
@@ -4,8 +4,6 @@ import LiteralExt from 'rdf-ext/lib/Literal'
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface FileLiteral extends Literal {}
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 export class FileLiteral extends LiteralExt {
   constructor (public file: File) {
     super(file.name)
