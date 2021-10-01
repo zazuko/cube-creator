edecf5a5cde48cca7ff9021406e2ec22f813f5a4
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2021-10-01 11:40:37 +0200

    fix: to not overwrite job uri

--- cli/test/lib/commands/timeoutJobs.test.ts
+++ cli/test/lib/commands/timeoutJobs.test.ts
@@ -15,11 +15,9 @@ describe('@cube-creator/cli/lib/commands/timeoutJobs', () => {
   it('sets failed status to active jobs which were not updated since given duration', async () => {
     // given
     const updateJob = sinon.spy()
-    const executionUrl = 'http://example.com/job'
 
     // when
     await timeoutJobs({
-      executionUrl,
       duration: 'PT6H',
       now() {
         return Date.parse('2020-10-29T20:01:54')
@@ -32,7 +30,6 @@ describe('@cube-creator/cli/lib/commands/timeoutJobs', () => {
     expect(updateJob).to.have.been.calledWithMatch({
       jobUri: 'https://cube-creator.lndo.site/cube-project/ubd/csv-mapping/jobs/hung-job',
       status: schema.FailedActionStatus,
-      executionUrl,
     })
   })
 })
