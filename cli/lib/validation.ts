c48274629f8db0013886ccdec5fd4f5d562ff209
Martin Maillard <git@martinmaillard.com>
2021-10-06 10:57:51 +0200

    chore: use rdf-string to serialize quad

--- cli/lib/validation.ts
+++ cli/lib/validation.ts
@@ -1,14 +1,19 @@
-import { Quad } from 'rdf-js'
-import $rdf from 'rdf-ext'
+import { turtle } from '@tpluscode/rdf-string'
+import { NamedNode, Quad } from 'rdf-js'
 import type { Context } from 'barnard59-core/lib/Pipeline'
 import { validateQuad } from 'rdf-validate-datatype'
 
 export function validate(this: Context, quad: Quad): Quad {
   if (!validateQuad(quad)) {
-    // Take advantage of rdf-ext's `toString`
-    const quadExt = $rdf.quad(quad.subject, quad.predicate, quad.object)
-    throw new Error(`Quad object has invalid datatype:\n\t${quadExt.toString()}`)
+    throw new Error(`Quad object has invalid datatype:\n\t${quadToString(quad)}`)
   }
 
   return quad
+}
+
+function quadToString(quad: Quad): string {
+  return turtle`${quad}`.toString({
+    directives: false, // do not print @prefix
+    graph: quad.graph as NamedNode,
+  })
 }
