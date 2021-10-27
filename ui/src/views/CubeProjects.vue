e997ce5a2ee90a5f80815e6d66e4aab3ea50e151
Martin Maillard <git@martinmaillard.com>
2021-10-27 13:45:16 +0200

    feat(ui): show project details

--- ui/src/views/CubeProjects.vue
+++ ui/src/views/CubeProjects.vue
@@ -17,31 +17,12 @@
     </div>
     <div v-if="projectsCollection">
       <div v-if="projects.length > 0" class="panel">
-        <router-link
+        <CubeProjectsItem
           v-for="project in projects"
           :key="project.id.value"
-          :to="{ name: 'CubeProject', params: { id: project.clientPath } }"
+          :project="project"
           class="panel-block"
-        >
-          <div class="is-flex-grow-1 is-flex is-justify-content-space-between">
-            <div>
-              <p class="has-text-weight-bold">
-                {{ project.label }}
-              </p>
-              <p class="is-size-7" title="Cube identifier">
-                {{ project.cubeIdentifier || (project.sourceCube && project.sourceCube.value) }}
-              </p>
-            </div>
-            <div v-if="project.maintainer" class="is-flex is-flex-direction-column is-align-items-flex-end">
-              <p class="tag">
-                {{ project.maintainer.displayLabel }}
-              </p>
-              <p class="is-size-7 pr-2" v-if="project.creator && project.creator.name">
-                {{ project.creator.name }}
-              </p>
-            </div>
-          </div>
-        </router-link>
+        />
       </div>
       <p v-else class="has-text-grey">
         No projects yet
@@ -56,13 +37,14 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import { ProjectsCollection, Project } from '@cube-creator/model'
+import CubeProjectsItem from '@/components/CubeProjectsItem.vue'
 import PageContent from '@/components/PageContent.vue'
 import LoadingBlock from '@/components/LoadingBlock.vue'
 import HydraOperationButton from '@/components/HydraOperationButton.vue'
 import * as storeNs from '../store/namespace'
 
 @Component({
-  components: { PageContent, LoadingBlock, HydraOperationButton },
+  components: { CubeProjectsItem, PageContent, LoadingBlock, HydraOperationButton },
 })
 export default class CubeProjectsView extends Vue {
   @storeNs.projects.State('collection') projectsCollection!: ProjectsCollection | null;
