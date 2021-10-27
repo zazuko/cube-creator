10147672e8e7590698db63423e8edc28277ed5e9
Martin Maillard <git@martinmaillard.com>
2021-10-27 13:49:24 +0200

    feat(ui): display datetime literals in local format

--- ui/src/components/TermDisplay.vue
+++ ui/src/components/TermDisplay.vue
@@ -6,8 +6,9 @@
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator'
-import { Term } from 'rdf-js'
+import { Literal, Term } from 'rdf-js'
 import { shrink } from '@/rdf-properties'
+import { xsd } from '@tpluscode/rdf-ns-builders'
 
 @Component
 export default class extends Vue {
@@ -41,7 +42,20 @@ export default class extends Vue {
       return shrink(this.term.value, this.base)
     }
 
+    if (this.term.termType === 'Literal') {
+      return formatLiteral(this.term)
+    }
+
     return this.term.value
   }
+}
+
+function formatLiteral (term: Literal): string {
+  console.log(term.datatype)
+  if (xsd.dateTime.equals(term.datatype)) {
+    return new Date(term.value).toLocaleString()
+  }
+
+  return term.value
 }
 </script>
