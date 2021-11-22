e283c2e8cb1f122e4f511941886df4662cba2dc4
Martin Maillard <git@martinmaillard.com>
2021-11-22 10:47:41 +0100

    fix: avoid duplicate contactPoints

--- apis/core/lib/domain/dataset/update.ts
+++ apis/core/lib/domain/dataset/update.ts
@@ -37,6 +37,7 @@ export async function update({
   datasetResource.deleteOut(cc.dimensionMetadata).addOut(cc.dimensionMetadata, dimensionMetadata.terms)
 
   // Set schema.org contact point
+  datasetResource.deleteOut(schema.contactPoint)
   datasetResource.out(dcat.contactPoint).forEach(contact => {
     datasetResource.addOut(schema.contactPoint, schemaContact => {
       schemaContact.addOut(rdf.type, schema.ContactPoint)
