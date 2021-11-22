79e43d0069223588d412da2ac283782051463110
Martin Maillard <git@martinmaillard.com>
2021-11-22 14:00:05 +0100

    feat: replace shared term URI with link button

---
+++ .changeset/tricky-zebras-trade.md
@@ -0,0 +1,5 @@
+---
+"@cube-creator/ui": patch
+---
+
+Replace shared term URI column with a button/link to the term (closes #964)
---
+++ ui/src/components/SharedDimensionTermLink.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-tooltip :label="uri">
+    <a :href="uri" target="_blank" rel="noopener noreferer" class="button is-small is-text">
+      <b-icon icon="external-link-alt" />
+    </a>
+  </b-tooltip>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import { SharedDimensionTerm } from '../store/types'
+
+@Component
+export default class extends Vue {
+  @Prop({ required: true }) term!: SharedDimensionTerm
+
+  get uri (): string {
+    return this.term.canonical?.value || this.term.id?.value
+  }
+}
+</script>
