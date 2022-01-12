5e7deb9999e7880a4199d37b830973b88aa221d8
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-01-12 12:18:13 +0100

    refactor: break up large cubes into multiple queries

--- cli/lib/loadCube.ts
+++ cli/lib/loadCube.ts
@@ -1,10 +1,12 @@
 import type * as Pipeline from 'barnard59-core/lib/Pipeline'
-import { Stream } from 'rdf-js'
-import { CONSTRUCT } from '@tpluscode/sparql-builder'
+import { Stream, PassThrough } from 'stream'
+import { CONSTRUCT, SELECT, sparql } from '@tpluscode/sparql-builder'
 import StreamClient from 'sparql-http-client/StreamClient'
+import ParsingClient from 'sparql-http-client/ParsingClient'
 import { csvw } from '@tpluscode/rdf-ns-builders'
 import { cube } from '@cube-creator/core/namespace'
 import { loadProject } from './project'
+import { fromRdf } from 'rdf-literal'
 
 interface Params {
   jobUri: string
@@ -13,6 +15,8 @@ interface Params {
   password: string
 }
 
+const LIMIT = 1000000
+
 /**
  * Opens a SPARQL stream reading the cube, excluding the Cube Constraint subgraph,
  * which will be processed alongside the rest of cube metadata
@@ -20,18 +24,55 @@ interface Params {
 export async function loadCube(this: Pipeline.Context, { jobUri, endpoint, user, password }: Params): Promise<Stream> {
   const project = await loadProject(jobUri, this)
 
-  const query = CONSTRUCT`?s ?p ?o`
-    .FROM(project.cubeGraph)
-    .WHERE`
-      ?s ?p ?o .
-      ?cube a ${cube.Cube} ; !${cube.observationConstraint}* ?s .
-
-      filter (?p != ${csvw.describes})
-    `
-
-  return query.execute(new StreamClient({
+  const clientConfig = {
     endpointUrl: endpoint,
     user,
     password,
-  }).query)
+  }
+  const client = new StreamClient(clientConfig)
+  const parsingClient = new ParsingClient(clientConfig)
+
+  const patterns = sparql`
+    ?s ?p ?o .
+    ?cube a ${cube.Cube} ; !${cube.observationConstraint}* ?s .
+
+    filter (?p != ${csvw.describes})`
+
+  const [{ count }] = await SELECT`(COUNT(*) as ?count)`
+    .FROM(project.cubeGraph)
+    .WHERE`${patterns}`
+    .execute(parsingClient.query) as any
+
+  const total = fromRdf(count)
+  const queries: ReturnType<typeof CONSTRUCT>[] = []
+  let offset = 0
+  while (offset < total) {
+    queries.push(await CONSTRUCT`?s ?p ?o`
+      .FROM(project.cubeGraph)
+      .WHERE`${patterns}`
+      .LIMIT(LIMIT)
+      .OFFSET(offset))
+
+    offset += LIMIT
+  }
+
+  const combined = new PassThrough({
+    objectMode: true,
+  })
+  const next = async () => {
+    const current = await queries.shift()?.execute(client.query)
+    if (current) {
+      this.logger.info('Fetching next cube chunk')
+      current.pipe(combined, {
+        end: false,
+      })
+      current.on('error', e => combined.destroy(e))
+      current.on('end', next)
+    } else {
+      combined.end()
+    }
+  }
+
+  next()
+  return combined
 }
