a17b7d44c0cf51a32c1107e98e0aadf5ca5240ae
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-01-25 16:06:07 +0100

    Save cube to file before publishing (#1062)

---
+++ .changeset/sweet-brooms-promise.md
@@ -0,0 +1,5 @@
+---
+"@cube-creator/cli": patch
+---
+
+Fetch entire cube to file first when publishing (re #756)
--- cli/lib/loadCube.ts
+++ cli/lib/loadCube.ts
@@ -1,10 +1,13 @@
 import type * as Pipeline from 'barnard59-core/lib/Pipeline'
 import { Stream, PassThrough } from 'stream'
 import { CONSTRUCT, sparql } from '@tpluscode/sparql-builder'
-import StreamClient from 'sparql-http-client/StreamClient'
 import { csvw } from '@tpluscode/rdf-ns-builders'
 import { cube } from '@cube-creator/core/namespace'
+import tempy from 'tempy'
+import { spawnSync } from 'child_process'
+import fromFile from 'rdf-utils-fs/fromFile'
 import { loadProject } from './project'
+import { tracer } from './otel/tracer'
 
 interface Params {
   jobUri: string
@@ -13,8 +16,6 @@ interface Params {
   password: string
 }
 
-const LIMIT = parseInt(process.env.CUBE_QUERY_LIMIT || '0', 10) || 1000000
-
 /**
  * Opens a SPARQL stream reading the cube, excluding the Cube Constraint subgraph,
  * which will be processed alongside the rest of cube metadata
@@ -22,46 +23,49 @@ const LIMIT = parseInt(process.env.CUBE_QUERY_LIMIT || '0', 10) || 1000000
 export async function loadCube(this: Pipeline.Context, { jobUri, endpoint, user, password }: Params): Promise<Stream> {
   const project = await loadProject(jobUri, this)
 
-  const client = new StreamClient({
-    endpointUrl: endpoint,
-    user,
-    password,
-  })
-
   const patterns = sparql`
     ?s ?p ?o .
     ?cube a ${cube.Cube} ; !${cube.observationConstraint}* ?s .
 
     filter (?p != ${csvw.describes})`
 
-  let offset = 0
-
   const combined = new PassThrough({
     objectMode: true,
   })
-  const next = async () => {
+
+  tempy.file.task(async tempFile => {
+    this.logger.info('Saving cube data to temp file %s', tempFile)
     const query = CONSTRUCT`?s ?p ?o`
       .FROM(project.cubeGraph)
       .WHERE`${patterns}`
-      .LIMIT(LIMIT)
-      .OFFSET(offset)
-    offset += LIMIT
+      .build()
 
-    let received = false
-    const current = await query.execute(client.query)
-    this.logger.info(`Fetching next cube chunk (OFFSET=${offset})`)
-    current.pipe(combined, {
-      end: false,
-    })
-    current.once('data', () => {
-      received = true
-    })
-    current.on('error', e => combined.destroy(e))
-    current.on('end', () => {
-      received ? next() : combined.end()
-    })
-  }
+    const params = new URLSearchParams({ query }).toString()
 
-  next()
+    tracer.startActiveSpan('download cube data', span => {
+      spawnSync('curl', [
+        endpoint,
+        '-X', 'POST',
+        '-u', `${user}:${password}`,
+        '--data', params,
+        '-H', 'Accept:text/turtle',
+        '-o', tempFile,
+      ],
+      { stdio: [process.stdin, process.stdout, process.stderr] })
+      span.end()
+    })
+
+    this.logger.info('Reading cube data to temp file')
+    const localQuads: any = fromFile(tempFile)
+    localQuads.pipe(combined)
+
+    return new Promise((resolve, reject) => {
+      localQuads.on('end', resolve)
+      localQuads.on('error', reject)
+    })
+  }, {
+    extension: 'ttl',
+  })
+
   return combined
 }
