e76d161413041c8adbf34ec560525497e223e098
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-03-18 08:57:07 +0100

    build(deps): keep one tree component

--- ui/src/shims-vue.d.ts
+++ ui/src/shims-vue.d.ts
@@ -1,4 +1,12 @@
+import { Store } from '@/store'
+
 declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
+}
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $store: Store;
+  }
 }
--- ui/types/vuex.d.ts
+++
@@ -1,8 +0,0 @@
-import { Store } from 'vuex'
-import { RootState } from '../src/store/types'
-
-declare module '@vue/runtime-core' {
-  export interface ComponentCustomProperties {
-    $store: Store<RootState>
-  }
-}
