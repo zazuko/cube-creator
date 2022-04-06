a47648319c08f7d5006d1d6219af44788747008b
Martin Maillard <git@martinmaillard.com>
2022-04-06 14:09:23 +0200

    chore: add missing handler file

---
+++ apis/shared-dimensions/lib/handlers/hierarchy.ts
@@ -0,0 +1,15 @@
+import { dcterms, sd } from '@tpluscode/rdf-ns-builders'
+import { asyncMiddleware } from 'middleware-async'
+import $rdf from 'rdf-ext'
+import env from '@cube-creator/core/env'
+
+export const get = asyncMiddleware(async (req, res) => {
+  const hierarchy = await req.hydra.resource.clownface()
+
+  hierarchy.addOut(dcterms.source, source => {
+    source
+      .addOut(sd.endpoint, $rdf.namedNode(env.PUBLIC_QUERY_ENDPOINT))
+  })
+
+  return res.dataset(hierarchy.dataset)
+})
