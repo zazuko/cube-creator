382bd415d7219bfdd2b7484e979131f58ec970e3
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-05-09 16:29:02 +0200

    fix: do not rewrite hierarchy roots

--- apis/shared-dimensions/lib/middleware/canonicalRewrite.ts
+++ apis/shared-dimensions/lib/middleware/canonicalRewrite.ts
@@ -15,15 +15,36 @@ export function rewriteTerm<T extends Term>(term: T): T {
   return term
 }
 
+export interface ShouldRewrite {
+  (quad: Quad): true | { [p in keyof Quad]?: false }
+}
+
+const defaultShouldRewrite: ShouldRewrite = ({ predicate }) => {
+  if (predicate.equals(oa.canonical)) {
+    return {
+      subject: false,
+    }
+  }
+
+  return true
+}
+
 /**
  * Rewrites quad to change canonical URIs with API namespace with the exception of oa:canonical property
  */
-function rewriteQuad({ subject, predicate, object, graph }: Quad): Quad {
+function rewriteQuad(quad: Quad, shouldRewriteCheck: ShouldRewrite = defaultShouldRewrite): Quad {
+  const shouldRewrite = shouldRewriteCheck(quad)
+
+  const rewriteSubject = shouldRewrite === true || shouldRewrite.subject !== false
+  const rewritePredicate = shouldRewrite === true || shouldRewrite.predicate !== false
+  const rewriteObject = shouldRewrite === true || shouldRewrite.object !== false
+  const rewriteGraph = shouldRewrite === true || shouldRewrite.graph !== false
+
   return $rdf.quad(
-    rewriteTerm(subject),
-    rewriteTerm(predicate),
-    predicate.equals(oa.canonical) ? object : rewriteTerm(object),
-    rewriteTerm(graph),
+    rewriteSubject ? rewriteTerm(quad.subject) : quad.subject,
+    rewritePredicate ? rewriteTerm(quad.predicate) : quad.predicate,
+    rewriteObject ? rewriteTerm(quad.object) : quad.object,
+    rewriteGraph ? rewriteTerm(quad.graph) : quad.graph,
   )
 }
 
@@ -31,14 +52,18 @@ export const patchResponseStream: express.RequestHandler = asyncMiddleware(async
   const { quadStream } = res
 
   res.quadStream = (stream: Stream & Readable) => {
-    if (res.locals.noRewrite) {
-      // when set, res.locals.noRewrite prevents the rewrite of
+    if (res.locals.noRewrite === true) {
+      // when set to true, res.locals.noRewrite prevents the rewrite of
       // canonical terms to cube-creator URIs
       return quadStream(stream)
     }
 
     const rewriteTerms = through2.obj(function (chunk: Quad, enc, callback) {
-      this.push(rewriteQuad(chunk))
+      if (typeof res.locals.noRewrite === 'function') {
+        this.push(rewriteQuad(chunk, res.locals.noRewrite))
+      } else {
+        this.push(rewriteQuad(chunk))
+      }
 
       callback()
     })
