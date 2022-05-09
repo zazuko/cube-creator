cc7a9bec49684fbd4681647efaa16246308ea195
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-05-09 16:29:02 +0200

    fix: do not rewrite hierarchy roots

---
+++ .changeset/yellow-peaches-pull.md
@@ -0,0 +1,5 @@
+---
+"@cube-creator/shared-dimensions-api": patch
+---
+
+Hierarchy roots would be shown in wrong domain (cube-creator instead of `ld.admin.ch`)
--- apis/shared-dimensions/lib/handlers/hierarchy.ts
+++ apis/shared-dimensions/lib/handlers/hierarchy.ts
@@ -1,7 +1,10 @@
+import { Quad } from 'rdf-js'
 import { dcterms, sd } from '@tpluscode/rdf-ns-builders'
 import { asyncMiddleware } from 'middleware-async'
 import $rdf from 'rdf-ext'
 import env from '@cube-creator/core/env'
+import { meta } from '@cube-creator/core/namespace'
+import { ShouldRewrite } from '../middleware/canonicalRewrite'
 
 export const get = asyncMiddleware(async (req, res) => {
   const hierarchy = await req.hydra.resource.clownface()
@@ -12,6 +15,18 @@ export const get = asyncMiddleware(async (req, res) => {
         .addOut(sd.endpoint, $rdf.namedNode(env.PUBLIC_QUERY_ENDPOINT))
     })
   }
+
+  const noRewriteRoots: ShouldRewrite = (quad: Quad) => {
+    if (quad.predicate.equals(meta.hierarchyRoot)) {
+      return {
+        object: false,
+      }
+    }
+
+    return true
+  }
+
+  res.locals.noRewrite = noRewriteRoots
 
   return res.dataset(hierarchy.dataset)
 })
