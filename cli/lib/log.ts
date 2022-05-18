b6f2d73ebea09c23a48b91392415ce7fb12a7eb2
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-05-18 09:28:24 +0200

    chore: prettier pipeline log

--- cli/lib/log.ts
+++ cli/lib/log.ts
@@ -1,4 +1,6 @@
 import winston from 'winston'
+import { OnViolation } from 'barnard59-validate-shacl'
+import { fromPointer } from '@rdfine/shacl/lib/ValidationReport'
 
 export const logger = winston.createLogger({
   format: winston.format.simple(),
@@ -6,3 +8,10 @@ export const logger = winston.createLogger({
     new winston.transports.Console(),
   ],
 })
+
+export function prettyPrintReport({ context, report }: OnViolation) {
+  const jsonld = fromPointer(report.pointer).toJSON()
+  context.logger.error(JSON.stringify(jsonld, null, 2))
+
+  return false
+}
