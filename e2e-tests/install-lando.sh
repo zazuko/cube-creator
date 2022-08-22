b3715786d118f2ac3c9a90b00c4ec1eb0e4c909a
tpluscode <tpluscode@users.noreply.github.com>
2022-08-22 09:28:09 +0200

    ci: remove boilerplate to set up lando

--- e2e-tests/install-lando.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-curl -fsSL -o /tmp/lando-latest.deb https://github.com/lando/lando/releases/download/v3.6.2/lando-x64-v3.6.2.deb
-sudo dpkg -i /tmp/lando-latest.deb
-lando version
--- e2e-tests/start-lando.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-lando start
-npx wait-on --timeout 30000 https://cube-creator.lndo.site/ping
