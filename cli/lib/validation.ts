5d90026193f1253702db4317c4e574067227dc48
Giacomo Citi <giacomo.citi@zazuko.com>
2024-05-16 10:57:26 +0200

    improve datatype error messages

--- cli/lib/validation.ts
+++ cli/lib/validation.ts
@@ -1,11 +1,11 @@
-import type { NamedNode, Quad } from '@rdfjs/types'
+import type { Literal, NamedNode, Quad } from '@rdfjs/types'
 import { turtle } from '@tpluscode/rdf-string'
 import type { Context } from 'barnard59-core/lib/Pipeline'
 import { validateQuad } from 'rdf-validate-datatype'
 
 export function validate(this: Context, quad: Quad): Quad {
   if (!validateQuad(quad)) {
-    throw new Error(`Quad object has invalid datatype:\n\t${quadToString(quad)}`)
+    throw new Error(`${errorMessage(quad)}\n\n${quadToString(quad)}`)
   }
 
   return quad
@@ -16,4 +16,13 @@ function quadToString(quad: Quad): string {
     directives: false, // do not print @prefix
     graph: quad.graph as NamedNode,
   })
+}
+
+function errorMessage(quad: Quad): string {
+  const literal = quad.object as Literal
+  if (!literal.datatype) {
+    return `Invalid datatype for non-literal ${literal.value}`
+  }
+
+  return `literal "${literal.value}" is not a valid value for datatype <${literal.datatype.value}>.`
 }
