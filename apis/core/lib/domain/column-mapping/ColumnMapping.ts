3719ba91b1066e1b58b40c7448e553df4cb37e8c
Giacomo Citi <giacomo.citi@zazuko.com>
2024-05-27 09:18:01 +0200

    revert message for error (prevented by shape)

--- apis/core/lib/domain/column-mapping/ColumnMapping.ts
+++ apis/core/lib/domain/column-mapping/ColumnMapping.ts
@@ -1,6 +1,5 @@
 import type { NamedNode } from '@rdfjs/types'
 import { Constructor } from '@tpluscode/rdfine'
-import { xsd } from '@tpluscode/rdf-ns-builders'
 import { Initializer, RdfResourceCore } from '@tpluscode/rdfine/RdfResource'
 import { cc } from '@cube-creator/core/namespace'
 import * as Schema from '@rdfine/schema'
@@ -27,16 +26,7 @@ declare module '@cube-creator/model' {
 export function LiteralColumnMappingMixin<Base extends Constructor<Omit<LiteralColumnMapping, keyof ApiColumnMapping>>>(Resource: Base): Constructor<ApiColumnMapping & RdfResourceCore> & Base {
   return class extends Resource implements ApiColumnMapping {
     setErrors() {
-      delete this.errors
-      const errors: Array<Initializer<Schema.Thing>> = []
-
-      if (this.language && this.datatype && !xsd.string.equals(this.datatype)) {
-        errors.push({
-          description: 'either set language or datatype, not both',
-        })
-      }
-
-      this.errors = errors as any
+      //
     }
   }
 }
