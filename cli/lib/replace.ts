bb137febb5917bfc6f05454867f903ddb7c14657
Giacomo Citi <giacomo.citi@zazuko.com>
2024-09-26 17:29:51 +0200

    use async generator

--- cli/lib/replace.ts
+++ cli/lib/replace.ts
@@ -1,19 +1,19 @@
-import through2 from 'through2'
 import type { Quad } from '@rdfjs/types'
-import $rdf from 'rdf-ext'
+import { Context } from 'barnard59-core'
 
-function replace(version: string) {
+export default function replace(this: Context, version: string) {
   const prefix = 'https://cube.link/latest/'
   const replacement = `https://cube.link/${version}/`
-  return through2.obj(function (quad: Quad, _, done) {
-    if (quad.predicate.value === 'https://code.described.at/imports' && quad.object.value.startsWith(prefix)) {
-      const replaced = quad.object.value.replace(prefix, replacement)
-      const triple = $rdf.triple(quad.subject, quad.predicate, $rdf.namedNode(replaced))
-      done(null, triple)
-    } else {
-      done(null, quad)
-    }
-  })
-}
+  const { env } = this
 
-export default replace
+  return async function * (stream: AsyncIterable<Quad>) {
+    for await (const quad of stream) {
+      if (quad.predicate.equals(env.ns.code.imports) && quad.object.value.startsWith(prefix)) {
+        const replaced = quad.object.value.replace(prefix, replacement)
+        yield env.quad(quad.subject, quad.predicate, env.namedNode(replaced))
+      } else {
+        yield quad
+      }
+    }
+  }
+}
